f2cfe7fbed52c447613f0d29389096f28e0b6a15
Khai Do <zaro0508@gmail.com>
2017-10-17 20:11:32 -0700

    Setup fastlane to build and deploy android app
    Setup fastlane to build and deploy similar to mpower-android[1] repo.
    The main difference is that this project will use keystores and keys
    from the android-certificates[2] repo for generating a signed apk.
    
    [1] https://github.com/Sage-Bionetworks/mPower-Android
    [2] https://github.com/Sage-Bionetworks/android-certificates

---
+++ travis/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -ex
+if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then     # pull request
+    bundle exec fastlane test
+elif [[ -z "$TRAVIS_TAG" && "$TRAVIS_BRANCH" == "master" ]]; then  # non-tag commits to master branch
+    bundle exec fastlane test
+    bundle exec fastlane assemble
+elif [[ -z "$TRAVIS_TAG" && "$TRAVIS_BRANCH" =~ ^stable-.* ]]; then # non-tag commits to stable branches
+    ./setup_git_crypt.sh
+    openssl aes-256-cbc -K $encrypted_89e57280a3a1_key -iv $encrypted_89e57280a3a1_iv -in git-crypt-android-certificates.key.enc -out git-crypt-android-certificates.key -d
+    git clone https://github.com/Sage-Bionetworks/android-certificates ../android-certificates
+    pushd ../android-certificates
+    /tmp/git-crypt-master/git-crypt unlock $TRAVIS_BUILD_DIR/git-crypt-android-certificates.key
+    popd
+    bundle exec fastlane test
+    bundle exec fastlane alpha alias:"$KEY_ALIAS" storepass:"$KEYSTORE_PASSWORD" keypass:"$KEY_PASSWORD" signed_apk_path:"app/build/outputs/apk/app-release.apk"
+fi
+exit $?
