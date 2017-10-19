b053a6686ec4e9e6af01c1fe27842aeef026d3a6
Khai Do <zaro0508@gmail.com>
2017-10-19 16:14:50 -0700

    install a specific build of git-crypt

--- setup_git_crypt.sh
+++ setup_git_crypt.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 pushd /tmp
-wget https://github.com/AGWA/git-crypt/archive/master.zip -O git-crypt.zip
+wget https://github.com/AGWA/git-crypt/archive/ccdcc76f8e1a639847a8accd801f5a284194e43f.zip -O git-crypt.zip
 unzip git-crypt.zip
 cd git-crypt-master
 make
