8b7c40e2cc40ebeff4b79bf37ebee0a415ed4778
MDP <mike@sdpdigital.com>
2017-10-20 17:47:42 -0400

    Added task helper override

--- app/src/main/java/org/sagebionetworks/bridge/researchstack/CrfDataProvider.java
+++ app/src/main/java/org/sagebionetworks/bridge/researchstack/CrfDataProvider.java
@@ -3,8 +3,12 @@ package org.sagebionetworks.bridge.researchstack;
 import android.content.Context;
 
 import org.researchstack.backbone.DataResponse;
+import org.researchstack.backbone.ResourceManager;
 import org.researchstack.backbone.result.TaskResult;
+import org.researchstack.backbone.storage.NotificationHelper;
+import org.researchstack.skin.AppPrefs;
 import org.sagebionetworks.bridge.android.manager.BridgeManagerProvider;
+import org.sagebionetworks.bridge.researchstack.wrapper.StorageAccessWrapper;
 
 import rx.Observable;
 import rx.functions.Action0;
@@ -18,6 +22,11 @@ public class CrfDataProvider extends BridgeDataProvider {
     public CrfDataProvider() {
         // TODO give path to permission file for uploads
         super(BridgeManagerProvider.getInstance());
+    }
+
+    @Override
+    public TaskHelper createTaskHelper(NotificationHelper notif, StorageAccessWrapper wrapper, BridgeManagerProvider provider) {
+        return new CrfTaskHelper(wrapper, ResourceManager.getInstance(), AppPrefs.getInstance(), notif, provider);
     }
 
     @Override
