d0e4e2dc95a07d1c526fa0e35da50cb6727dd987
MDP <mike@sdpdigital.com>
2017-11-27 20:10:56 -0500

    Added "Step 1 of 3" type textview to enhance the progress view

--- app/src/main/java/org/sagebase/crf/CrfActiveTaskActivity.java
+++ app/src/main/java/org/sagebase/crf/CrfActiveTaskActivity.java
@@ -21,22 +21,29 @@ import android.content.Context;
 import android.content.Intent;
 import android.media.AudioManager;
 import android.os.Build;
+import android.os.Bundle;
 import android.support.annotation.ColorRes;
 import android.support.annotation.DrawableRes;
 import android.support.annotation.IdRes;
 import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.ActionBar;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.style.StyleSpan;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 import org.researchstack.backbone.step.Step;
+import org.researchstack.backbone.step.active.ActiveStep;
 import org.researchstack.backbone.task.OrderedTask;
 import org.researchstack.backbone.task.Task;
 import org.researchstack.backbone.ui.ActiveTaskActivity;
 
+import org.researchstack.backbone.ui.step.layout.ActiveStepLayout;
 import org.researchstack.backbone.ui.step.layout.StepLayout;
 import org.researchstack.backbone.utils.ResUtils;
 import org.sagebase.crf.step.CrfResultListener;
@@ -45,6 +52,7 @@ import org.sagebase.crf.view.CrfTaskStatusBarManipulator;
 import org.sagebase.crf.view.CrfTaskToolbarActionManipulator;
 import org.sagebase.crf.view.CrfTaskToolbarIconManipulator;
 import org.sagebase.crf.view.CrfTaskToolbarProgressManipulator;
+import org.sagebase.crf.view.CrfTaskToolbarTintManipulator;
 import org.sagebase.crf.view.CrfTransparentToolbar;
 import org.sagebionetworks.research.crf.R;
 
@@ -67,6 +75,8 @@ public class CrfActiveTaskActivity extends ActiveTaskActivity {
         return null;
     }
 
+    private TextView crfStepProgressTextview;
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         boolean status = super.onCreateOptionsMenu(menu);
@@ -79,6 +89,12 @@ public class CrfActiveTaskActivity extends ActiveTaskActivity {
         storageAccessUnregister();
         MainApplication.mockAuthenticate(this);
         super.onDataReady();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        crfStepProgressTextview = findViewById(R.id.crf_step_progress_textview);
     }
 
     @Override
@@ -126,8 +142,25 @@ public class CrfActiveTaskActivity extends ActiveTaskActivity {
                 R.drawable.crf_ic_close,
                 CrfTaskToolbarIconManipulator.NO_ICON);
 
+        // The text color of the step progress defaults to white,
+        // but is set to a darker theme for all tint colors other than white
+        @ColorRes int stepProgressTextColorRes = R.color.white;
+        if (current instanceof CrfTaskToolbarTintManipulator) {
+            if (((CrfTaskToolbarTintManipulator)current).crfToolbarTintColor() != R.color.white) {
+                stepProgressTextColorRes = R.color.darkGrayText;
+            }
+        }
+        int stepProgressTextColor = ResourcesCompat.getColor(getResources(), stepProgressTextColorRes, null);
+        crfStepProgressTextview.setTextColor(stepProgressTextColor);
+
+        // Set the visibility of the step progress text to mimic the progress bar visibility
         if (!(current instanceof CrfTaskToolbarProgressManipulator)) {
             crfToolbar.showProgressInToolbar(true);
+            crfStepProgressTextview.setVisibility(View.VISIBLE);
+        } else if (((CrfTaskToolbarProgressManipulator)current).crfToolbarShowProgress()) {
+            crfStepProgressTextview.setVisibility(View.VISIBLE);
+        } else {
+            crfStepProgressTextview.setVisibility(View.GONE);
         }
 
         // Allow for customization of the status bar
@@ -147,6 +180,19 @@ public class CrfActiveTaskActivity extends ActiveTaskActivity {
             int progress = orderedTask.getSteps().indexOf(currentStep);
             int max = orderedTask.getSteps().size();
             crfToolbar.setProgress(progress, max);
+
+            // Set up the text and styling of the step 1 of 5, 2 of 5, etc.
+            String progressStr = String.valueOf(progress);
+            String maxString = String.valueOf(max);
+            String stepProgressStr = String.format(getString(R.string.crf_step_progress), progressStr, maxString);
+
+            SpannableStringBuilder str = new SpannableStringBuilder(stepProgressStr);
+            str.setSpan(new StyleSpan(android.graphics.Typeface.BOLD),
+                    stepProgressStr.indexOf(progressStr),
+                    stepProgressStr.indexOf(progressStr) + progressStr.length(),
+                    Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+            crfStepProgressTextview.setText(str);
         } else {
             Log.e("CrfActiveTaskActivity", "Progress Bars only work with OrderedTask");
         }
