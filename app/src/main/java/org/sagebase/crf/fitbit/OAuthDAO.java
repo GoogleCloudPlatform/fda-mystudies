728e3ce73657a1253da655e051433af0dc7886e5
Joshua Liu <liujoshua@users.noreply.github.com>
2018-02-09 10:18:29 -0800

    Save tester/participant DataGroup step

---
+++ app/src/main/java/org/sagebase/crf/fitbit/OAuthDAO.java
@@ -0,0 +1,10 @@
+package org.sagebase.crf.fitbit;
+
+import org.sagebionetworks.bridge.android.manager.dao.SharedPreferencesJsonDAO;
+
+/**
+ * Created by liujoshua on 2/9/2018.
+ */
+
+public class OAuthDAO extends SharedPreferencesJsonDAO {
+}
