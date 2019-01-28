48d7f23418557177603be554a961f660e864c137
Aishwarya Mandyam <aishwarya_m@live.com>
2019-01-28 11:07:15 -0800

    Trying to set the text color to white

--- app/src/main/java/org/sagebase/crf/step/CrfSkipMCStepLayout.java
+++ app/src/main/java/org/sagebase/crf/step/CrfSkipMCStepLayout.java
@@ -17,17 +17,32 @@
 
 package org.sagebase.crf.step;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import org.researchstack.backbone.factory.IntentFactory;
 import org.researchstack.backbone.result.StepResult;
+import org.researchstack.backbone.step.FormStep;
 import org.researchstack.backbone.step.Step;
+import org.researchstack.backbone.task.Task;
+import org.researchstack.backbone.ui.step.layout.SurveyStepLayout;
+import org.sagebase.crf.CrfSurveyTaskActivity;
+import org.sagebase.crf.reminder.CrfReminderManager;
+import org.sagebionetworks.bridge.researchstack.CrfResourceManager;
+import org.sagebionetworks.bridge.researchstack.CrfTaskFactory;
 import org.sagebionetworks.research.crf.R;
 
 public class CrfSkipMCStepLayout extends CrfFormStepLayout {
 
     protected CrfSkipMCStep step;
-    protected TextView crf_instruction;
+    protected TextView crf_title;
+    protected TextView crf_text;
+    protected Button remindMeLaterButton;
 
     public CrfSkipMCStepLayout(Context context) {
         super(context);
@@ -39,7 +54,7 @@ public class CrfSkipMCStepLayout extends CrfFormStepLayout {
         super.initialize(step, result);
         setupViews();
         refreshCrfSubmitBar();
-        //this.crf_instruction = findViewById(R.id.rsb_form_step_title);
+
         this.step = (CrfSkipMCStep) step;
     }
 
@@ -51,7 +66,57 @@ public class CrfSkipMCStepLayout extends CrfFormStepLayout {
     @Override
     public void setupViews() {
         super.setupViews();
-        //crf_instruction.setText("Hello World"); // not sure if this is the right title to display
 
+        this.crf_title = findViewById(R.id.rsb_survey_title);
+        this.crf_text = findViewById(R.id.rsb_survey_text);
+        this.remindMeLaterButton = findViewById(R.id.remind_me_later);
+
+    }
+
+    @Override
+    protected void initStepLayout(FormStep step)
+    {
+        super.initStepLayout(step);
+        container.setBackgroundColor(Color.parseColor("#C94281"));
+
+    }
+
+    @Override
+    protected void refreshCrfSubmitBar() {
+        super.refreshCrfSubmitBar();
+
+        crfNextButton.setText("Done");
+        crfBackButton.setVisibility(GONE);
+
+        crf_title.setTextColor(Color.rgb(255, 255, 255));
+        crf_title.setVisibility(VISIBLE);
+        crf_text.setTextColor(Color.parseColor("#FFFFFF"));
+        crf_text.setVisibility(VISIBLE);
+
+
+        /*if (remindMeLaterButton != null) {
+            if (step.remindMeLater) {
+                remindMeLaterButton.setVisibility(View.VISIBLE);
+                remindMeLaterButton.setOnClickListener(new OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        remindMeLater();
+                    }
+                });
+            } else {
+                remindMeLaterButton.setVisibility(View.GONE);
+            }
+        }*/
+    }
+
+    public void remindMeLater() {
+        Task task = (new CrfTaskFactory()).createTask(getContext(), CrfResourceManager.REMIND_ME_LATER_RESOURCE);
+        Intent intent = IntentFactory.INSTANCE.newTaskIntent(getContext(), CrfSurveyTaskActivity.class, task);
+        if (!(callbacks instanceof Activity)) {
+            throw new IllegalStateException("Callbacks class must be an activity " +
+                    "so we can start another activity from this step layout");
+        }
+        Activity activity = (Activity)callbacks;
+        activity.startActivityForResult(intent, CrfReminderManager.DAILY_REMINDER_REQUEST_CODE);
     }
 }
