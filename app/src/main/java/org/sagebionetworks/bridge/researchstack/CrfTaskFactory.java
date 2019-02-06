018be19f005f4228a18b4ce0ddd52ccf90e88472
Aishwarya Mandyam <aishwarya_m@live.com>
2019-02-06 15:07:28 -0800

    Changed text to black

--- app/src/main/java/org/sagebionetworks/bridge/researchstack/CrfTaskFactory.java
+++ app/src/main/java/org/sagebionetworks/bridge/researchstack/CrfTaskFactory.java
@@ -274,6 +274,9 @@ public class CrfTaskFactory extends TaskItemFactory {
         if (item.tintColorRes != null) {
             step.tintColorRes = item.tintColorRes;
         }
+        if (item.textColor != null) {
+            step.textColor = item.textColor;
+        }
         if (item.statusBarColorRes != null) {
             step.statusBarColorRes = item.statusBarColorRes;
         }
@@ -292,6 +295,7 @@ public class CrfTaskFactory extends TaskItemFactory {
         else {
             step.remindMeLater = false;
         }
+
     }
 
     private CrfStartTaskStep createCrfStartTaskStep(CrfStartTaskSurveyItem item) {
