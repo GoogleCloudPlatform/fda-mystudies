a044645881ac2175eb12faaa1158234c9f02623f
Aishwarya Mandyam <aishwarya_m@live.com>
2019-03-15 11:19:02 -0700

    Training task is up to date
    This reverts commit 627b1ef23b09c22bc41e95923f0cd481ad6d6668.

--- app/src/main/java/org/sagebase/crf/step/CrfHeartRateStepLayout.java
+++ app/src/main/java/org/sagebase/crf/step/CrfHeartRateStepLayout.java
@@ -25,8 +25,6 @@ import android.graphics.SurfaceTexture;
 import android.os.Build;
 import android.support.annotation.Nullable;
 import android.support.annotation.UiThread;
-import android.support.constraint.ConstraintLayout;
-import android.support.constraint.solver.widgets.ConstraintAnchor;
 import android.support.v4.content.res.ResourcesCompat;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -39,10 +37,8 @@ import android.view.animation.Animation;
 import android.view.animation.Transformation;
 import android.widget.Button;
 import android.widget.FrameLayout;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import org.researchstack.backbone.answerformat.DecimalAnswerFormat;
@@ -92,7 +88,6 @@ public class CrfHeartRateStepLayout extends ActiveStepLayout implements
         CrfResultListener {
     private static final Logger LOG = LoggerFactory.getLogger(CrfHeartRateStepLayout.class);
 
-    protected CrfHeartRateCameraStep step;
     private static final String AVERAGE_BPM_IDENTIFIER = "AVERAGE_BPM_IDENTIFIER";
     private static final String BPM_START_IDENTIFIER_SUFFIX = ".heartRate_start";
     private static final String BPM_END_IDENTIFIER_SUFFIX = ".heartRate_end";
@@ -117,19 +112,11 @@ public class CrfHeartRateStepLayout extends ActiveStepLayout implements
     protected View arcDrawableContainer;
     protected View arcDrawableView;
     protected ArcDrawable arcDrawable;
-    protected RelativeLayout layout;
-    protected RelativeLayout exitButtonContainer;
-    protected ConstraintLayout buttonContainer;
 
     protected Button nextButton;
     protected Button redoButton;
     protected ImageView heartImageView;
     protected HeartBeatAnimation heartBeatAnimation;
-
-    protected ImageButton exitButton;
-    protected ImageView crfCompletionIcon;
-    protected TextView crfPractice;
-    protected TextView coverFlash;
 
     // The previousBpm comes from the TaskResult of an unrelated CrfHeartRateStepLayout
     private int previousBpm = -1;
@@ -175,7 +162,6 @@ public class CrfHeartRateStepLayout extends ActiveStepLayout implements
 
     @Override
     public void initialize(Step step, StepResult result) {
-        this.step = (CrfHeartRateCameraStep) step;
         super.initialize(step, result);
     }
 
@@ -228,8 +214,6 @@ public class CrfHeartRateStepLayout extends ActiveStepLayout implements
         arcDrawable.setFullCirclePreviewColor(ResourcesCompat.getColor(getResources(), R.color.silver, null));
         arcDrawableView.setBackground(arcDrawable);
 
-        layout = findViewById(R.id.crf_root_instruction_layout);
-
         heartContainer = findViewById(R.id.crf_heart_container);
         heartContainer.setBackgroundColor(getResources().getColor(R.color.white));
 
@@ -246,10 +230,6 @@ public class CrfHeartRateStepLayout extends ActiveStepLayout implements
 
         heartImageView = findViewById(R.id.crf_heart_icon);
         heartImageView.setVisibility(View.GONE);
-
-        exitButton = findViewById(R.id.x_button);
-        exitButtonContainer = findViewById(R.id.exit_button_container);
-        buttonContainer = findViewById(R.id.crf_next_button_container);
 
         if (((CrfHeartRateCameraStep)activeStep).buttonType != null) {
             switch (((CrfHeartRateCameraStep)activeStep).buttonType) {
@@ -275,20 +255,6 @@ public class CrfHeartRateStepLayout extends ActiveStepLayout implements
             }
         }
 
-        if(this.exitButton != null) {
-            exitButton.setImageResource(R.drawable.x_light);
-            exitButton.setVisibility(View.VISIBLE);
-            exitButton.setOnClickListener(this::goBackClicked);
-        }
-
-        crfCompletionIcon = findViewById(R.id.crf_completion_icon);
-        crfPractice = findViewById(R.id.crf_practice);
-        coverFlash = findViewById(R.id.crf_later_tests);
-
-    }
-
-    private void goBackClicked(View view) {
-        callbacks.onSaveStep(StepCallbacks.ACTION_END, step, null);
     }
 
     // Wait for intelligent start to call super.start()
@@ -460,16 +426,10 @@ public class CrfHeartRateStepLayout extends ActiveStepLayout implements
 
 
     protected void showCompleteUi() {
-        //crfOops.setVisibility(View.INVISIBLE);
-        crfOops.setText("Nicely done!");
-        crfOops.setVisibility(View.VISIBLE);
+        crfOops.setVisibility(View.GONE);
         nextButton.setVisibility(View.VISIBLE);
         heartRateTextContainer.setVisibility(View.VISIBLE);
         bpmText.setTextColor(getResources().getColor(R.color.black86));
-        //crfMessageTextView.setText("Nicely done!");
-        //crfMessageTextView.setVisibility(View.VISIBLE);
-        calculateSuccess.setVisibility(View.VISIBLE);
-        arcDrawableContainer.setVisibility(View.VISIBLE);
 
 
 
@@ -499,28 +459,20 @@ public class CrfHeartRateStepLayout extends ActiveStepLayout implements
 
     private void showFinishUi() {
         shouldShowFinishUi = false;
-        layout.setBackgroundColor(getResources().getColor(R.color.mint));
-        exitButtonContainer.setBackgroundColor(getResources().getColor(R.color.mint));
-        buttonContainer.setBackgroundColor(getResources().getColor(R.color.white));
-
-        crfCompletionIcon.setVisibility(View.VISIBLE);
-        crfPractice.setVisibility(View.VISIBLE);
-        coverFlash.setVisibility(View.VISIBLE);
-
+        crfMessageTextView.setText("Nicely done!");
+        crfMessageTextView.setVisibility(View.VISIBLE);
         crfOops.setVisibility(View.INVISIBLE);
-        calculateSuccess.setVisibility(View.INVISIBLE);
-        heartContainer.setVisibility(View.GONE);
 
+        calculateSuccess.setVisibility(View.VISIBLE);
 
-
-        nextButton.setText("Done");
-        nextButton.setOnClickListener(view -> onNextButtonClicked());
+        nextButton.setText("Try Again");
+        nextButton.setOnClickListener(view -> onRedoButtonClicked());
 
         redoButton.setVisibility(View.VISIBLE);
-        redoButton.setText(R.string.redo);
-        redoButton.setOnClickListener(view -> onRedoButtonClicked());
+        redoButton.setText(R.string.crf_done_underline);
+        redoButton.setOnClickListener(view -> onNextButtonClicked());
 
-        arcDrawableContainer.setVisibility(View.GONE);
+        arcDrawableContainer.setVisibility(View.VISIBLE);
 
     }
 
