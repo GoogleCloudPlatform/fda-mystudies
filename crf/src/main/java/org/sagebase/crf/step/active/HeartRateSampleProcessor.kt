4105d42159b07ec7e938f2c1492c4a6bbf37c3c3
syoung-smallwisdom <shannon.young@sagebase.org>
2019-04-17 23:12:05 -0700

    Port sample filtering from Swift to Kotlin

--- crf/src/main/java/org/sagebase/crf/step/active/HeartRateSampleProcessor.kt
+++ crf/src/main/java/org/sagebase/crf/step/active/HeartRateSampleProcessor.kt
@@ -31,6 +31,7 @@ import org.sagebase.crf.linearAlgebra.*
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //
 
+
 /// Frame rates supported by this processor, with the preferred framerate listed first.
 val SUPPORTED_FRAME_RATES = arrayOf(30)
 
@@ -152,4 +153,156 @@ class HeartRateSampleProcessor @JvmOverloads constructor(val videoProcessorFrame
         }
         return output
     }
+
+    // --- Code ported from Swift implementation (CRF - SageResearch), originally validated in R. syoung 04/17/2019
+
+    fun chunkSamples(input: List<Double>, samplingRate: Double, window: Double = HEART_RATE_WINDOW_IN_SECONDS) : List<List<Double>> {
+        //    hr.data.filtered.chunks <- hr.data.filtered %>%
+        //    dplyr::select(red, green, blue) %>%
+        //    na.omit() %>%
+        //    lapply(mhealthtools:::window_signal, window_length, window_overlap, 'rectangle')
+        var output: MutableList<List<Double>> = mutableListOf()
+        val windowLength = Math.round(window * samplingRate).toInt()
+        var start: Int = 0
+        var end: Int = windowLength
+        while (end < input.size) {
+            output.add(input.subList(start, end))
+            start += Math.round(samplingRate).toInt()
+            end = start + windowLength
+        }
+        return output
+    }
+
+    ///#' Bandpass and sorted mean filter the given signal
+    ///#'
+    ///#' @param x A time series numeric data
+    ///#' @param sampling_rate The sampling rate (fs) of the signal
+    fun getFilteredSignal(input: List<Double>, samplingRate: Int, dropSeconds: Int = 0) : DoubleArray {
+        //        x[is.na(x)] <- 0
+        //        x <- x[round(3*sampling_rate):length(x)]
+        //        # Ignore the first 3s
+        // - note: syoung 04/16/2019 There is a small round-off error in the sample prefiltering code where
+        // the first 3 seconds minus 1 is what should actually be dropped. During task run, this is ignored
+        // (because the 3 seconds is removed by checking for the isLensCovered flag), but to match his output
+        // it is reproduced here for testing purposes.
+        val drop = if (dropSeconds > 0) dropSeconds * samplingRate - 1 else 0
+        val x = input.drop(drop).map { if (it.isFinite()) it else 0.0 }
+        //    x <- signal::filter(bf_low, x) # lowpass
+        //    x <- x[round(sampling_rate):length(x)] # 1s
+        val lowpass = passFilter(x.toDoubleArray(), samplingRate = samplingRate, type = FilterType.low).drop(samplingRate)
+        //    x <- signal::filter(bf_high, x) # highpass
+        //    x <- x[round(sampling_rate):length(x)] # 1s @ 60Hz
+        val highpass = passFilter(lowpass.toDoubleArray(), samplingRate = samplingRate, type = FilterType.high).drop(samplingRate)
+        // filter using mean centering
+        val filtered = meanCenteringFilter(highpass.toDoubleArray(), samplingRate = samplingRate)
+        return filtered
+    }
+
+    fun autocorrelation(input: DoubleArray, lagMax: Int = 80) : DoubleArray {
+        //    x_acf <- rep(0, lag.max+1) # the +1 is because, we will have a 0 lag value also
+        //    xl <- length(x) # total no of samples
+        //    mx <- mean(x) # average/mean
+        //    varx <- sum((x-mx)^2) # Unscaled variance
+        //    for(i in seq(0, lag.max)){ # for i=0:lag.max
+        //        x_acf[i+1] <- sum( (x[1:(xl-i)]-mx) * (x[(i+1):xl]-mx))/varx // # (Unscaled Co-variance)/(Unscaled variance)
+        val xl = input.size
+        val mx = input.average()
+        val varx: Double = input.reduce() { sum, element -> sum + (element - mx) * (element - mx) }
+        val x = input.offsetR()
+        val x_acf = DoubleArray(lagMax + 1) {
+            val x_left = x.copyOfRange(1, (xl - it) + 1)
+            val x_right = x.copyOfRange((it + 1), xl + 1)
+            var sum: Double = 0.0
+            for (idx in 0 until x_left.size) {
+                sum += (x_left[idx] - mx) * (x_right[idx] - mx)
+            }
+            sum / varx
+        }
+        return x_acf
+    }
+
+    fun meanCenteringFilter(input: DoubleArray, samplingRate: Int) : DoubleArray {
+        val mean_filter_order = meanFilterOrder(samplingRate)
+        val lowerBounds = (mean_filter_order + 1) / 2
+        val upperBounds = input.size - (mean_filter_order - 1) / 2
+        val x = input.offsetR()
+        val range = IntArray(upperBounds - lowerBounds + 1) { it ->
+            (it + lowerBounds)
+        }
+        val y = range.map { i  ->
+            val tempLower: Int = i - (mean_filter_order - 1) / 2
+            val tempUpper: Int = i + (mean_filter_order - 1) / 2
+            val temp_sequence = x.copyOfRange(tempLower, tempUpper + 1)
+            val temp_sum = temp_sequence.sum()
+            val temp_max = temp_sequence.max()!!
+            val temp_min = temp_sequence.min()!!
+            val temp_minus = (temp_sum - temp_max + temp_min) / (mean_filter_order - 2).toDouble()
+            // # 0.00001 is a small value, ideally this should be machine epsilon
+            ((x[i] - temp_minus) / (temp_max - temp_min + 0.00001))
+        }
+        return y.toDoubleArray()
+    }
+
+    // TODO: syoung 04/17/2019 Need to figure out how to call the CsvUtils method. What's a context?
+    val lowPassParameters: Map<Int, CsvUtils.PassFilterParams> = HashMap<Int, CsvUtils.PassFilterParams>()
+    val highPassParameters: Map<Int, CsvUtils.PassFilterParams> = HashMap<Int, CsvUtils.PassFilterParams>()
+
+    fun passFilter(input: DoubleArray, samplingRate: Int, type: FilterType) : DoubleArray{
+        val paramArray = if ((type == FilterType.low)) lowPassParameters else highPassParameters
+        val params = paramArray[samplingRate]
+        if (params == null) {
+            throw Exception("WARNING! Failed to get butterworth filter coeffients for ${samplingRate}")
+        }
+        // insert 0 at the first element to offset the array to the 1 index
+        val b = params.bParams.offsetR()
+        val a = params.aParams.offsetR()
+        val x = input.offsetR()
+        //    y <- rep(0, xl)
+        //    # Create a sequence of 0s of the same length as x
+        var y = DoubleArray(x.size)
+
+        //Array(repeating = Double(0), count = x.size)
+        //    # For index i, less than the length of the filter we have
+        //    # a[1] = 1, always, as the filter coeffs are normalized
+        y[1] = b[1] * x[1]
+        y[2] = b[1] * x[2] + b[2] * x[1] - a[2] * y[1]
+        y[3] = b[1] * x[3] + b[2] * x[2] + b[3] * x[1] - a[2] * y[2] - a[3] * y[1]
+        y[4] = b[1] * x[4] + b[2] * x[3] + b[3] * x[2] + b[4] * x[1] - a[2] * y[3] - a[3] * y[2] - a[4] * y[1]
+        y[5] = b[1] * x[5] + b[2] * x[4] + b[3] * x[3] + b[4] * x[2] + b[5] * x[1] - a[2] * y[4] - a[3] * y[3] - a[4] * y[2] - a[5] * y[1]
+        y[6] = b[1] * x[6] + b[2] * x[5] + b[3] * x[4] + b[4] * x[3] + b[5] * x[2] + b[6] * x[1] - a[2] * y[5] - a[3] * y[4] - a[4] * y[3] - a[5] * y[2] - a[6] * y[1]
+        y[7] = b[1] * x[7] + b[2] * x[6] + b[3] * x[5] + b[4] * x[4] + b[5] * x[3] + b[6] * x[2] + b[7] * x[1] - a[2] * y[6] - a[3] * y[5] - a[4] * y[4] - a[5] * y[3] - a[6] * y[2] - a[7] * y[1]
+        //    # For index i, greater than or equal to the length of the filter, we have
+        //    for(i in seq(8,length(x))){
+        for (i in 8 until x.size) {
+            y[i] = b[1] * x[i] + b[2] * x[i - 1] + b[3] * x[i - 2] + b[4] * x[i - 3] + b[5] * x[i - 4] + b[6] * x[i - 5] + b[7] * x[i - 6] + b[8] * x[i - 7] - a[2] * y[i - 1] - a[3] * y[i - 2] - a[4] * y[i - 3] - a[5] * y[i - 4] - a[6] * y[i - 5] - a[7] * y[i - 6] - a[8] * y[i - 7]
+        }
+        return y.drop(1).toDoubleArray()
+    }
+
+    fun meanFilterOrder(samplingRate: Int) : Int {
+        if ((samplingRate <= 32)) {
+            return 33
+        } else if ((samplingRate <= 18)) {
+            return 19
+        } else if ((samplingRate <= 15)) {
+            return 15
+        } else {
+            return 65
+        }
+    }
 }
+
+enum class FilterType {
+    low, high
+}
+
+/**
+ * R is indexed from 1 so to simplify porting the code, everything is offset by one
+ * in the filters before calculating.
+ */
+internal fun DoubleArray.offsetR() : DoubleArray {
+    var list = this.toMutableList()
+    list.add(0, 0.0)
+    return list.toDoubleArray()
+}
+
