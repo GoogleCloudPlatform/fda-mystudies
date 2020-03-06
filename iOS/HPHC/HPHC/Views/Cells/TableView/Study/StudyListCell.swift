638fdf01626c2a7bc91033a3c15ceda264815d63
Tushar Katyal <tushark@boston-technology.com>
2020-03-06 19:10:30 +0530

    - Update copyrights.

--- iOS/HPHC/HPHC/Views/Cells/TableView/Study/StudyListCell.swift
+++ iOS/HPHC/HPHC/Views/Cells/TableView/Study/StudyListCell.swift
@@ -1,13 +1,8 @@
 //  HPHC
-//  Copyright 2020 Google LLC
-//
-//  Use of this source code is governed by an MIT-style
-//  license that can be found in the LICENSE file or at
-//  https://opensource.org/licenses/MIT.
-//
 // License Agreement for FDA My Studies
-// Copyright © 2017-2019 Harvard Pilgrim Health Care Institute (HPHCI) and its Contributors. Permission is
-// hereby granted, free of charge, to any person obtaining a copy of this software and associated
+// Copyright © 2017-2019 Harvard Pilgrim Health Care Institute (HPHCI) and its Contributors.
+// Copyright 2020 Google LLC
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
 // documentation files (the &quot;Software&quot;), to deal in the Software without restriction, including without
 // limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
 // Software, and to permit persons to whom the Software is furnished to do so, subject to the following
