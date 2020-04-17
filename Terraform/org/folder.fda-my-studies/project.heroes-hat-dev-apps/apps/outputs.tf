493f634fd157d22c8d0de8938d55d55db3c7770a
Martin Petkov <mpetkov@google.com>
2020-04-17 09:52:26 -0400

    Move GKE SAs out, they're a separate PR now
    PR: https://github.com/GoogleCloudPlatform/fda-mystudies/pull/168

--- Terraform/org/folder.fda-my-studies/project.heroes-hat-dev-apps/apps/outputs.tf
+++ Terraform/org/folder.fda-my-studies/project.heroes-hat-dev-apps/apps/outputs.tf
@@ -15,11 +15,3 @@
 output "service_account" {
   value = module.heroes_hat_cluster.service_account
 }
-
-output "gke_cluster" {
-  value = module.heroes_hat_cluster
-}
-
-output "apps_service_accounts" {
-  value = google_service_account.apps_service_accounts
-}
