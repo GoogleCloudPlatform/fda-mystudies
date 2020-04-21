690cc0f48ee796ebad53070ea6fed9b766e685ac
Xin <xingao267@users.noreply.github.com>
2020-04-21 15:37:13 -0400

    Comment out customer triggers initially. (#199)
    Add more permissions.

--- Terraform/org/folder.fda-my-studies/project.heroes-hat-dev-apps/apps/cloudbuild.tf
+++ Terraform/org/folder.fda-my-studies/project.heroes-hat-dev-apps/apps/cloudbuild.tf
@@ -18,112 +18,115 @@
 
 # Run after install the Cloud Build app and connect GitHub repo in Cloud Build by following
 # https://cloud.google.com/cloud-build/docs/automating-builds/create-github-app-triggers#installing_the_cloud_build_app.
-resource "google_cloudbuild_trigger" "wcp" {
-  # Disable for now as don't know when the owners want to generate new images.
-  disabled = true
-  provider = google-beta
-  project  = var.project_id
-  name     = "wcp"
 
-  included_files = [
-    "WCP/**"
-  ]
+# TODO: Uncomment the following after connect the repo in Cloud Build.
 
-  github {
-    owner = var.repo_owner
-    name  = var.repo_name
-    push {
-      branch = var.cloudbuild_trigger_branch
-    }
-  }
+# resource "google_cloudbuild_trigger" "wcp" {
+#   # Disable for now as don't know when the owners want to generate new images.
+#   disabled = true
+#   provider = google-beta
+#   project  = var.project_id
+#   name     = "wcp"
 
-  filename = "WCP/cloudbuild.yaml"
-}
+#   included_files = [
+#     "WCP/**"
+#   ]
 
-resource "google_cloudbuild_trigger" "auth_server_ws" {
-  # Disable for now as don't know when the owners want to generate new images.
-  disabled = true
-  provider = google-beta
-  project  = var.project_id
-  name     = "auth-server-ws"
+#   github {
+#     owner = var.repo_owner
+#     name  = var.repo_name
+#     push {
+#       branch = var.cloudbuild_trigger_branch
+#     }
+#   }
 
-  included_files = [
-    "auth-server-ws/**"
-  ]
+#   filename = "WCP/cloudbuild.yaml"
+# }
 
-  github {
-    owner = var.repo_owner
-    name  = var.repo_name
-    push {
-      branch = var.cloudbuild_trigger_branch
-    }
-  }
+# resource "google_cloudbuild_trigger" "auth_server_ws" {
+#   # Disable for now as don't know when the owners want to generate new images.
+#   disabled = true
+#   provider = google-beta
+#   project  = var.project_id
+#   name     = "auth-server-ws"
 
-  filename = "auth-server-ws/cloudbuild.yaml"
-}
+#   included_files = [
+#     "auth-server-ws/**"
+#   ]
 
-resource "google_cloudbuild_trigger" "wcp_ws" {
-  # Disable for now as don't know when the owners want to generate new images.
-  disabled = true
-  provider = google-beta
-  project  = var.project_id
-  name     = "wcp-ws"
+#   github {
+#     owner = var.repo_owner
+#     name  = var.repo_name
+#     push {
+#       branch = var.cloudbuild_trigger_branch
+#     }
+#   }
 
-  included_files = [
-    "WCP-WS/**"
-  ]
+#   filename = "auth-server-ws/cloudbuild.yaml"
+# }
 
-  github {
-    owner = var.repo_owner
-    name  = var.repo_name
-    push {
-      branch = var.cloudbuild_trigger_branch
-    }
-  }
+# resource "google_cloudbuild_trigger" "wcp_ws" {
+#   # Disable for now as don't know when the owners want to generate new images.
+#   disabled = true
+#   provider = google-beta
+#   project  = var.project_id
+#   name     = "wcp-ws"
 
-  filename = "WCP-WS/cloudbuild.yaml"
-}
+#   included_files = [
+#     "WCP-WS/**"
+#   ]
 
-resource "google_cloudbuild_trigger" "user_registration_server_ws" {
-  # Disable for now as don't know when the owners want to generate new images.
-  disabled = true
-  provider = google-beta
-  project  = var.project_id
-  name     = "user-registration-server-ws"
+#   github {
+#     owner = var.repo_owner
+#     name  = var.repo_name
+#     push {
+#       branch = var.cloudbuild_trigger_branch
+#     }
+#   }
 
-  included_files = [
-    "user-registration-server-ws/**"
-  ]
+#   filename = "WCP-WS/cloudbuild.yaml"
+# }
 
-  github {
-    owner = var.repo_owner
-    name  = var.repo_name
-    push {
-      branch = var.cloudbuild_trigger_branch
-    }
-  }
+# resource "google_cloudbuild_trigger" "user_registration_server_ws" {
+#   # Disable for now as don't know when the owners want to generate new images.
+#   disabled = true
+#   provider = google-beta
+#   project  = var.project_id
+#   name     = "user-registration-server-ws"
 
-  filename = "user-registration-server-ws/cloudbuild.yaml"
-}
+#   included_files = [
+#     "user-registration-server-ws/**"
+#   ]
 
-resource "google_cloudbuild_trigger" "response_server_ws" {
-  # Disable for now as don't know when the owners want to generate new images.
-  disabled = true
-  provider = google-beta
-  project  = var.project_id
-  name     = "response-server-ws"
+#   github {
+#     owner = var.repo_owner
+#     name  = var.repo_name
+#     push {
+#       branch = var.cloudbuild_trigger_branch
+#     }
+#   }
 
-  included_files = [
-    "response-server-ws/**"
-  ]
+#   filename = "user-registration-server-ws/cloudbuild.yaml"
+# }
 
-  github {
-    owner = var.repo_owner
-    name  = var.repo_name
-    push {
-      branch = var.cloudbuild_trigger_branch
-    }
-  }
+# resource "google_cloudbuild_trigger" "response_server_ws" {
+#   # Disable for now as don't know when the owners want to generate new images.
+#   disabled = true
+#   provider = google-beta
+#   project  = var.project_id
+#   name     = "response-server-ws"
 
-  filename = "response-server-ws/cloudbuild.yaml"
-}
+#   included_files = [
+#     "response-server-ws/**"
+#   ]
+
+#   github {
+#     owner = var.repo_owner
+#     name  = var.repo_name
+#     push {
+#       branch = var.cloudbuild_trigger_branch
+#     }
+#   }
+
+#   filename = "response-server-ws/cloudbuild.yaml"
+# }
