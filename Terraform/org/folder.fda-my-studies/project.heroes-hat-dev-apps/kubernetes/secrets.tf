97706a1f8d38e330e43c9c2280285a5a45d6e0cd
Martin Petkov <mpetkov@google.com>
2020-04-23 14:47:48 -0400

    Move provider block to main.tf

--- Terraform/org/folder.fda-my-studies/project.heroes-hat-dev-apps/kubernetes/secrets.tf
+++ Terraform/org/folder.fda-my-studies/project.heroes-hat-dev-apps/kubernetes/secrets.tf
@@ -1,12 +1,3 @@
-data "google_client_config" "default" {}
-
-provider "kubernetes" {
-  load_config_file       = false
-  host                   = var.my_studies_cluster.endpoint
-  token                  = data.google_client_config.default.access_token
-  cluster_ca_certificate = base64decode(var.my_studies_cluster.ca_certificate)
-}
-
 locals {
   apps = [
     "auth-server",
