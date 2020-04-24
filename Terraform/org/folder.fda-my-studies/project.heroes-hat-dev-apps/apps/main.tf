00c0383336819bf12df5040397681a65129d4a39
Martin Petkov <mpetkov@google.com>
2020-04-24 14:37:25 -0400

    Add Istio prometheus images to BinAuthZ
    It seems like the more generic pattern is not enough for all Istio images.

--- Terraform/org/folder.fda-my-studies/project.heroes-hat-dev-apps/apps/main.tf
+++ Terraform/org/folder.fda-my-studies/project.heroes-hat-dev-apps/apps/main.tf
@@ -95,6 +95,10 @@ resource "google_binary_authorization_policy" "policy" {
   admission_whitelist_patterns {
     name_pattern = "gke.gcr.io/istio/*"
   }
+  admission_whitelist_patterns {
+    # The more generic pattern above does not seem to be enough for all images.
+    name_pattern = "gke.gcr.io/istio/prometheus/*"
+  }
 
   # Allow Google-built images.
   # See https://cloud.google.com/binary-authorization/docs/policy-yaml-reference#globalpolicyevaluationmode
