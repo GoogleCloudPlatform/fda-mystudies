d80cbc2f02e0f8346a395fbdca9a636247dc46ce
Yu-chi Kuo <yuchikuo@google.com>
2020-04-29 16:44:03 -0700

    Add comment

--- Terraform/copy_client_info_to_sql.sh
+++ Terraform/copy_client_info_to_sql.sh
@@ -6,8 +6,10 @@ DATA_PROJECT=heroes-hat-dev-data
 SQL_IMPORT_BUCKET=heroes-hat-dev-data-sql-import
 TMPFILE=$(mktemp)
 
+# Write auth server db name to TMPFILE.
 echo "USE \`auth_server\`;" >> ${TMPFILE}
 
+# Loop over all APP_CODE to read secret and write to TMPFILE.
 i=0
 for APP_CODE in MA URS RS WCP
 do
@@ -18,9 +20,11 @@ do
   ((i=i+1))
 done
 
+# Upload TMPFILE to GCS.
 GCS_FILE=gs://${SQL_IMPORT_BUCKET}/auth_server_client_info.sql
 echo "Copying the sql file to ${GCS_FILE}"
 gsutil mv ${TMPFILE} ${GCS_FILE}
 
+# Import the GCS file to CloudSQL.
 echo "Importing ${GCS_FILE} to CloudSQL."
 gcloud sql import sql --project=${DATA_PROJECT} my-studies ${GCS_FILE}
