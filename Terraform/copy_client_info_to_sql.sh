51ed39d6ed886e747acf7d59299df7cf8a626264
Yu-chi Kuo <yuchikuo@google.com>
2020-04-29 16:41:14 -0700

    Add script to copy client info from secrets to CloudSQL

---
+++ Terraform/copy_client_info_to_sql.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Script to copy client id and secret keys from gcloud secret to CloudSQL.
+
+SECRET_PROJECT=heroes-hat-dev-devops
+DATA_PROJECT=heroes-hat-dev-data
+SQL_IMPORT_BUCKET=heroes-hat-dev-data-sql-import
+TMPFILE=$(mktemp)
+
+echo "USE \`auth_server\`;" >> ${TMPFILE}
+
+i=0
+for APP_CODE in MA URS RS WCP
+do
+  echo "Reading client id and secret key for: ${APP_CODE}"
+  CLIENT_ID=`gcloud --project=${SECRET_PROJECT} secrets versions access latest --secret=mystudies-${APP_CODE,,}-client-id`
+  SECRET_KEY=`gcloud --project=${SECRET_PROJECT} secrets versions access latest --secret=mystudies-${APP_CODE,,}-secret-key`
+  echo "INSERT INTO client_info (client_info_id,app_code,client_id,secret_key) VALUES(${i},\"${APP_CODE}\",\"${CLIENT_ID}\",\"${SECRET_KEY}\");" >> ${TMPFILE}
+  ((i=i+1))
+done
+
+GCS_FILE=gs://${SQL_IMPORT_BUCKET}/auth_server_client_info.sql
+echo "Copying the sql file to ${GCS_FILE}"
+gsutil mv ${TMPFILE} ${GCS_FILE}
+
+echo "Importing ${GCS_FILE} to CloudSQL."
+gcloud sql import sql --project=${DATA_PROJECT} my-studies ${GCS_FILE}
