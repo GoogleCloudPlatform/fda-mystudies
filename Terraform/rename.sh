a5079cfb348fb36f0d61e81c343e555329b5f329
Zohreh Jabbari <zjabbari@google.com>
2020-05-28 18:10:20 -0400

    Limit to phase 1

--- Terraform/rename.sh
+++ Terraform/rename.sh
@@ -81,8 +81,8 @@ do
 done
 
 # Deployment Phase 2 - Uncomment after Phase 1 is deployed
-cp -r ${SRC_PROJ_BASE}-networks/networks ${DST_PROJ_BASE}-networks/
-cp -r ${SRC_PROJ_BASE}-resp-firebase/firebase ${DST_PROJ_BASE}-resp-firebase/
+# cp -r ${SRC_PROJ_BASE}-networks/networks ${DST_PROJ_BASE}-networks/
+# cp -r ${SRC_PROJ_BASE}-resp-firebase/firebase ${DST_PROJ_BASE}-resp-firebase/
 
 # Deployment Phase 3 - Uncomment after Phase 1, 2 are deployed
 # cp -r ${SRC_PROJ_BASE}-apps/apps ${DST_PROJ_BASE}-apps/
