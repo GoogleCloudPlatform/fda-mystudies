b5e1ed857248080baa1c127033b31e535da507d3
firdoseb <firdoseb@boston-technology.com>
2020-05-29 16:58:20 +0530

    review suggestions has been updated

--- UR-web-app/src/polyfills.ts
+++ UR-web-app/src/polyfills.ts
@@ -14,8 +14,8 @@
  * Learn more in https://angular.io/guide/browser-support
  */
 
-/** *************************************************************************************************
- * BROWSER POLYFILLS
+/**
+ BROWSER POLYFILLS
  */
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
@@ -52,11 +52,11 @@ import 'web-animations-js'; // Run `npm install --save web-animations-js`.
  *
  */
 
-/** *************************************************************************************************
- * Zone JS is required by default for Angular itself.
+/**
+ Zone JS is required by default for Angular itself.
  */
 import 'zone.js/dist/zone'; // Included with Angular CLI.
 
-/** *************************************************************************************************
- * APPLICATION IMPORTS
+/**
+ APPLICATION IMPORTS
  */
