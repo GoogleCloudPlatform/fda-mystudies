bb3712b682c37b2e42b60080d2f0cae38527f1e7
Zohreh Jabbari <zjabbari@google.com>
2020-05-29 20:51:46 -0400

    remove firestore location and index

--- Terraform/org/folder.fda-my-studies/project.heroes-hat-dev-resp-firebase/firebase/main.tf
+++ Terraform/org/folder.fda-my-studies/project.heroes-hat-dev-resp-firebase/firebase/main.tf
@@ -29,29 +29,29 @@ resource "google_firebase_project" "firebase" {
   project  = var.project_id
 }
 
-resource "google_firebase_project_location" "basic" {
-  provider = google-beta
-  project  = google_firebase_project.firebase.project
+// resource "google_firebase_project_location" "basic" {
+//   provider = google-beta
+//   project  = google_firebase_project.firebase.project
 
-  location_id = "us-east1"
-}
+//   location_id = "us-east1"
+// }
 
-resource "google_firestore_index" "activities_index" {
-  project    = var.project_id
-  collection = "Activities"
-  fields {
-    field_path = "participantId"
-    order      = "ASCENDING"
-  }
-  fields {
-    field_path = "createdTimestamp"
-    order      = "ASCENDING"
-  }
-  fields {
-    field_path = "__name__"
-    order      = "ASCENDING"
-  }
-}
+// resource "google_firestore_index" "activities_index" {
+//   project    = var.project_id
+//   collection = "Activities"
+//   fields {
+//     field_path = "participantId"
+//     order      = "ASCENDING"
+//   }
+//   fields {
+//     field_path = "createdTimestamp"
+//     order      = "ASCENDING"
+//   }
+//   fields {
+//     field_path = "__name__"
+//     order      = "ASCENDING"
+//   }
+// }
 
 module "survey_pubsub" {
   source  = "terraform-google-modules/pubsub/google"
