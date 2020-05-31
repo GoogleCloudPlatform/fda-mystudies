1c503bebf49ae7ccc010ad8822ef5fd5b9971ffe
Zohreh Jabbari <zjabbari@google.com>
2020-05-30 21:30:16 -0400

    Remove firestore pubsub

--- Terraform/org/folder.fda-my-studies/project.heroes-hat-dev-resp-firebase/firebase/main.tf
+++ Terraform/org/folder.fda-my-studies/project.heroes-hat-dev-resp-firebase/firebase/main.tf
@@ -29,40 +29,11 @@ resource "google_firebase_project" "firebase" {
   project  = var.project_id
 }
 
-// resource "google_firebase_project_location" "basic" {
-//   provider = google-beta
-//   project  = google_firebase_project.firebase.project
+# resource "google_firebase_project_location" "basic" {
+#   provider = google-beta
+#   project  = google_firebase_project.firebase.project
+#
+#   location_id = "us-east1"
+# }
 
-//   location_id = "us-east1"
-// }
-
-// resource "google_firestore_index" "activities_index" {
-//   project    = var.project_id
-//   collection = "Activities"
-//   fields {
-//     field_path = "participantId"
-//     order      = "ASCENDING"
-//   }
-//   fields {
-//     field_path = "createdTimestamp"
-//     order      = "ASCENDING"
-//   }
-//   fields {
-//     field_path = "__name__"
-//     order      = "ASCENDING"
-//   }
-// }
-
-module "survey_pubsub" {
-  source  = "terraform-google-modules/pubsub/google"
-  version = "~> 1.0"
-
-  topic      = "surveyWriteTrigger"
-  project_id = var.project_id
-  pull_subscriptions = [
-    {
-      name                 = "surveyPHQDep"
-      ack_deadline_seconds = 10
-    }
-  ]
-}
+# Other firestore configurations, e.g. index, location, pubsub can be added here. 
