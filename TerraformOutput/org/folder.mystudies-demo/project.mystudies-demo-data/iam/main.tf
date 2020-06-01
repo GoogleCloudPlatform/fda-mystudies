b1b4f93821c65b5383ea8bd05e4a0b0ceb76afc4
Zohreh Jabbari <zjabbari@google.com>
2020-06-01 12:28:23 -0400

    Fix iam_members

--- TerraformOutput/org/folder.mystudies-demo/project.mystudies-demo-data/iam/main.tf
+++ TerraformOutput/org/folder.mystudies-demo/project.mystudies-demo-data/iam/main.tf
@@ -24,10 +24,10 @@ terraform {
   backend "gcs" {}
 }
 
-// resource "google_project_iam_member" "sql_clients" {
-//   for_each = toset(var.sql_clients)
+resource "google_project_iam_member" "sql_clients" {
+  for_each = toset(var.sql_clients)
 
-//   project = var.project_id
-//   role    = "roles/cloudsql.client"
-//   member  = each.key
-// }
+  project = var.project_id
+  role    = "roles/cloudsql.client"
+  member  = each.key
+}
