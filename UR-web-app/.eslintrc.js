756c19c022e65fa71cad82d7f12063c8e5c550fa
firdoseb <firdoseb@boston-technology.com>
2020-06-03 19:29:07 +0530

    code review comments has been resolved

--- UR-web-app/.eslintrc.js
+++ UR-web-app/.eslintrc.js
@@ -1,21 +1,20 @@
 module.exports = {
-  'env': {
+  env: {
     browser: true,
     es6: true,
   },
-  'extends': ['google'],
-  'globals': {
+  extends: ['google'],
+  globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
   },
-  'parser': '@typescript-eslint/parser',
-  'parserOptions': {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
     ecmaVersion: 11,
     sourceType: 'module',
   },
-  'plugins': ['@typescript-eslint'],
-
-  'rules': {
+  plugins: ['@typescript-eslint'],
+  rules: {
     'linebreak-style': 0,
     'require-jsdoc': 0,
     'no-unused-vars': 'off',
