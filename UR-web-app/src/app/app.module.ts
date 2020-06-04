6c654e5f44c3210384edb2b266b0f21006962d5d
firdoseb <firdoseb@boston-technology.com>
2020-06-04 22:33:47 +0530

    dataservice has been implemented

--- UR-web-app/src/app/app.module.ts
+++ UR-web-app/src/app/app.module.ts
@@ -1,11 +1,11 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
-
+import {DataService} from './service/dataService';
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
