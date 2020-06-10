75b5c857cf733703be192b8f346c5bceb2e9e75a
Zohreh Jabbari <zjabbari@google.com>
2020-06-10 17:33:09 -0400

    Fix iOS app name, fix response server healthCheck  (#51)
    * Rename iOS app
    * Clean up healthCheck for response server, remove noisy log

--- response-server-ws/src/main/java/com/google/cloud/healthcare/fdamystudies/controller/HealthController.java
+++ response-server-ws/src/main/java/com/google/cloud/healthcare/fdamystudies/controller/HealthController.java
@@ -8,18 +8,13 @@
 
 package com.google.cloud.healthcare.fdamystudies.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthController {
-  private static final Logger logger = LoggerFactory.getLogger(HealthController.class);
-
-  @GetMapping("/health")
-  public String ping() {
-    logger.info(" Response PingController - ping()");
-    return "MyStudies Response Server is up!";
+  @GetMapping("/healthCheck")
+  public String healthCheck() {
+    return "OK!";
   }
 }
