f3cc0ff2be82aed15cd0d13c0b5cb00e3a887f3d
RohithDC <rohithd@boston-technology.com>
2020-06-11 21:20:24 +0530

    Updated PR suggestions

--- UR-web-app/src/app/http-interceptors/Auth-Interceptor.ts
+++ UR-web-app/src/app/http-interceptors/Auth-Interceptor.ts
@@ -7,29 +7,27 @@ import {
   HttpRequest,
 } from '@angular/common/http';
 import {finalize} from 'rxjs/operators';
-
+import {User} from '../entity/User';
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
   constructor(private auth: AuthService, private spinner: NgxSpinnerService) {}
-
-  intercept(req: HttpRequest<any>, next: HttpHandler) {
-    this.spinner.show();
-    const userId = this.auth.getUserId();
-    const authUserId = this.auth.getAuthUserId();
-    const authToken = this.auth.getAuthorizationToken();
-    if (userId != null && authUserId != null && authToken != null) {
-      const headers = req.headers
-          .set('Content-Type', 'application/json')
-          .set('userId', userId)
-          .set('authToken', authToken)
-          .set('authUserId', authUserId);
-      const authReq = req.clone({headers});
-      return next.handle(authReq).pipe(
-          finalize(() => {
-            this.spinner.hide();
-          }),
-      );
-    }
-    return next.handle(req);
-  }
+ user:User | undefined ;
+ intercept(req: HttpRequest<any>, next: HttpHandler) {
+   this.spinner.show();
+   this.user = this.auth.getUserDetails();
+   if (this.user != null ) {
+     const headers = req.headers
+         .set('Content-Type', 'application/json')
+         .set('userId', this.user.id.toString())
+         .set('authToken', this.user.authToken)
+         .set('authUserId', this.user.urAdminAuthId);
+     const authReq = req.clone({headers});
+     return next.handle(authReq).pipe(
+         finalize(() => {
+           this.spinner.hide();
+         }),
+     );
+   }
+   return next.handle(req);
+ }
 }
