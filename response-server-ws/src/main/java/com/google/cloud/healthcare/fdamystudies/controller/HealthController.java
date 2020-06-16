4cb80f9361816840293129490c8a8b4cc3c5ad13
Zohreh Jabbari <zjabbari@google.com>
2020-06-16 14:26:16 -0400

    Consistent response

--- response-server-ws/src/main/java/com/google/cloud/healthcare/fdamystudies/controller/HealthController.java
+++ response-server-ws/src/main/java/com/google/cloud/healthcare/fdamystudies/controller/HealthController.java
@@ -15,6 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthController {
   @GetMapping("/healthCheck")
   public String healthCheck() {
-    return "OK!";
+    return "200 OK!";
   }
 }
