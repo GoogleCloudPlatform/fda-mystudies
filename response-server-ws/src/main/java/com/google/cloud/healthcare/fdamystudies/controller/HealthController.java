2b94b19ff47ac2e69de50dcf4b1539f96b370d76
Zohreh Jabbari <zjabbari@google.com>
2020-06-16 15:01:27 -0400

    Response server uses healthCheck (#75)

--- response-server-ws/src/main/java/com/google/cloud/healthcare/fdamystudies/controller/HealthController.java
+++ response-server-ws/src/main/java/com/google/cloud/healthcare/fdamystudies/controller/HealthController.java
@@ -13,8 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthController {
-  @GetMapping("/health")
-  public String health() {
-    return "OK!";
+  @GetMapping("/healthCheck")
+  public String healthcheck() {
+    return "200 OK!";
   }
 }
