45ea67c9eaaffbf7f58452985cccf322d6b6d9ca
RohithDC <rohithd@boston-technology.com>
2020-06-24 18:05:01 +0530

    AddLocation Components and Test cases

--- UR-web-app/src/app/site-coordinator/location/add-location/add-location.component.spec.ts
+++ UR-web-app/src/app/site-coordinator/location/add-location/add-location.component.spec.ts
@@ -1,14 +1,17 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {async, ComponentFixture, TestBed, inject} from '@angular/core/testing';
 
 import {AddLocationComponent} from './add-location.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {Component} from '@angular/core';
 
 describe('AddLocationComponent', () => {
   let component: AddLocationComponent;
   let fixture: ComponentFixture<AddLocationComponent>;
-
+  let addLocations: AddLocationComponent;
   beforeEach(async(async () => {
     await TestBed.configureTestingModule({
       declarations: [AddLocationComponent],
+      imports: [FormsModule, ReactiveFormsModule],
     }).compileComponents();
   }));
 
@@ -17,8 +20,75 @@ describe('AddLocationComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
+  beforeEach(inject(
+    [AddLocationComponent],
+    (addLocationForm: AddLocationComponent) => {
+      addLocations = addLocationForm;
+    },
+  ));
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should send data on submit', () => {
+    addLocations.submitted.subscribe(
+      ({loationCustomId, loationName, locationDescription}) => {
+        expect(loationCustomId).toEqual(expectedloationCustomId);
+        expect(loationName).toEqual(expectedloationName);
+        expect(locationDescription).toEqual(expectedlocationDescription);
+      },
+    );
+
+    addLocations.onSubmit({
+      loationCustomId: expectedloationCustomId,
+      loationName: expectedloationName,
+      locationDescription: expectedlocationDescription,
+    });
+  });
 });
+
+// describe('Shallow', () => {
+//   let component: AddLocationComponent;
+//   let fixture: ComponentFixture<AddLocationComponent>;
+//   let element;
+
+//   let addLocations: AddLocationComponent;
+//   beforeEach(async(() => {
+//     await TestBed.configureTestingModule({
+//       declarations: [AddLocationComponent],
+//       imports: [FormsModule, ReactiveFormsModule],
+//     }).compileComponents();
+//   }));
+
+//   it('should send credentials on submit', () => {
+//     fixture = TestBed.createComponent(AddLocationComponent);
+//     component = fixture.componentInstance;
+//     const element = fixture.nativeElement;
+
+//     fixture.detectChanges();
+
+//     element.querySelector('#loationCustomId').value = expectedloationCustomId;
+//     element.querySelector('#loationCustomId').dispatchEvent(new Event('input'));
+//     element.querySelector('#loationName').value = expectedloationName;
+//     element.querySelector('#loationName').dispatchEvent(new Event('input'));
+//     element.querySelector(
+//       '#locationDescription',
+//     ).value = expectedlocationDescription;
+//     element
+//       .querySelector('#locationDescription')
+//       .dispatchEvent(new Event('input'));
+
+//     fixture.detectChanges();
+
+//     component.submitted.subscribe(
+//       ({loationCustomId, loationName, locationDescription}) => {
+//         expect(loationCustomId).toEqual(expectedloationCustomId);
+//         expect(loationName).toEqual(expectedloationName);
+//         expect(locationDescription).toEqual(expectedlocationDescription);
+//       },
+//     );
+
+//     element.querySelector('button[type="submit"]').click();
+//   });
+// });
--- UR-web-app/src/app/site-coordinator/location/add-location/add-location.component.ts
+++ UR-web-app/src/app/site-coordinator/location/add-location/add-location.component.ts
@@ -1,8 +1,46 @@
-import {Component} from '@angular/core';
+import {Component, OnInit, ViewChild, TemplateRef} from '@angular/core';
+import {Router} from '@angular/router';
+import {Location} from '../shared/location.model';
+import {LocationService} from '../shared/location.service';
+import {ToastrService} from 'ngx-toastr';
+import {BsModalService, BsModalRef} from 'ngx-bootstrap/modal';
+import {NgForm, NgModel} from '@angular/forms';
 
 @Component({
   selector: 'location-add',
   templateUrl: './add-location.component.html',
   styleUrls: ['./add-location.component.scss'],
 })
-export class AddLocationComponent {}
+export class AddLocationComponent implements OnInit {
+  @ViewChild('addLocationForm') addLocationForm: ngFormg;
+  location: Location = new Location();
+  errorMessage = '';
+  successMessage = '';
+  modalRef: BsModalRef;
+  constructor(
+    private router: Router,
+    private modalService: BsModalService,
+    private locationService: LocationService,
+    private toastr: ToastrService,
+  ) {}
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
+  }
+  ngOnInit() {}
+
+  addLocation() {
+    this.errorMessage = '';
+    this.successMessage = '';
+    this.locationService.addLocation(this.location).subscribe(
+      (data) => {
+        this.toastr.success(data.successBean.message);
+        // this.successMessage = "Location Added successfully."
+      },
+      (error) => {
+        console.log(error);
+        this.toastr.error(error.error.userMessage);
+        //this.errorMessage = error.error.message;
+      },
+    );
+  }
+}
