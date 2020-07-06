b43e5877a504b9d9de095b13dc736dc0cb38b2c7
Chiranjibi009 <chiranjibid@boston-technology.com>
2020-07-06 15:19:25 +0530

    Initial commit, healthCheck controller has been added with test cases

---
+++ user-registration-module/user-registration-service/src/main/java/com/google/cloud/healthcare/fdamystudies/config/AppConfig.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package com.google.cloud.healthcare.fdamystudies.config;
+
+import com.google.cloud.healthcare.fdamystudies.config.BaseAppConfig;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig extends BaseAppConfig {}
