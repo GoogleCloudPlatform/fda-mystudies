328ab5e9df28edfd99102aaee6c2be5fef83ccc4
RohithDC <rohithd@boston-technology.com>
2020-07-08 22:58:04 +0530

    Removed unused variable

--- UR-web-app/src/app/site-coordinator/location/add-location/add-location.component.ts
+++ UR-web-app/src/app/site-coordinator/location/add-location/add-location.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, EventEmitter, Output} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {Router} from '@angular/router';
 import {Location} from '../shared/location.model';
 import {LocationService} from '../shared/location.service';
