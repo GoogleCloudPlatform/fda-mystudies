db83bc0cdd1bb727d310ee3743f91daefd0f8baa
Tushar Katyal <tushark@boston-technology.com>
2020-07-14 12:58:18 +0530

    Remove delivered notifications on app launch.
    (cherry picked from commit 93ad55fad44bba46f00b3baac7e875e1734cdc47)

--- iOS/HPHC/HPHC/AppDelegate.swift
+++ iOS/HPHC/HPHC/AppDelegate.swift
@@ -259,7 +259,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     if number >= 1 {
       self.updateNotification()
     }
-
+    // Clear the delivered notifications when user enter's in the app.
+    DispatchQueue.main.async {
+      LocalNotification.removeAllDeliveredNotifications()
+    }
     // Check For Updates
     self.checkForAppUpdate()
   }
@@ -267,6 +270,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   func applicationDidBecomeActive(_ application: UIApplication) {
 
     UIApplication.shared.applicationIconBadgeNumber = 0
+    // Clear the delivered notifications when user enter's in the app.
+    DispatchQueue.main.async {
+      LocalNotification.removeAllDeliveredNotifications()
+    }
 
     if self.appIsResignedButDidNotEnteredBackground! {
 
