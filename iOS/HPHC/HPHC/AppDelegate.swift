571f3fef1ac79b61deb0470cdbaff67fe39a2b30
Tushar Katyal <tushark@boston-technology.com>
2020-07-15 11:32:01 +0530

    Remove ORKPasscodeStepViewController typecasting to avoid symbols issue.
    (cherry picked from commit 886660be84d48834a0ad4354aef37a91a6b5fb29)

--- iOS/HPHC/HPHC/AppDelegate.swift
+++ iOS/HPHC/HPHC/AppDelegate.swift
@@ -708,7 +708,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     guard
       ((viewController.presentedViewController as? ORKTaskViewController)?
-        .currentStepViewController as? ORKPasscodeStepViewController) == nil
+        .currentStepViewController)?.step?.identifier
+        != kPasscodeStepIdentifier
     else { return }  // If already presented. Return.
     if User.currentUser.userType == .loggedInUser {  // FDA user
 
@@ -935,7 +936,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   private func refreshStudyActivitiesState(with userInfo: JSONDictionary) {
     guard let currentStudyID = Study.currentStudy?.studyId,
       let studyID = userInfo["studyId"] as? String,
-      currentStudyID == studyID else { return }
+      currentStudyID == studyID
+    else { return }
     DispatchQueue.main.async {
       NotificationCenter.default.post(name: kRefreshActivities, object: nil)
     }
