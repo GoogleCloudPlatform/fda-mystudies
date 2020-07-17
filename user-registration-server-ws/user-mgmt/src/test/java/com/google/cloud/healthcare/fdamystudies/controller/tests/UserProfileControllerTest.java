ea21f0c3dd005e5e856fc9b4ac6b25295410da3d
aswinijena100 <ashwinikumarj@boston-technology.com>
2020-07-17 12:52:39 +0530

    integration test for updateprofile

--- user-registration-server-ws/user-mgmt/src/test/java/com/google/cloud/healthcare/fdamystudies/controller/tests/UserProfileControllerTest.java
+++ user-registration-server-ws/user-mgmt/src/test/java/com/google/cloud/healthcare/fdamystudies/controller/tests/UserProfileControllerTest.java
@@ -1,11 +1,17 @@
 package com.google.cloud.healthcare.fdamystudies.controller.tests;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.web.servlet.MvcResult;
 
+import com.google.cloud.healthcare.fdamystudies.beans.InfoBean;
+import com.google.cloud.healthcare.fdamystudies.beans.SettingsRespBean;
+import com.google.cloud.healthcare.fdamystudies.beans.UserRequestBean;
 import com.google.cloud.healthcare.fdamystudies.common.BaseMockIT;
 import com.google.cloud.healthcare.fdamystudies.config.AppConfig;
 import com.google.cloud.healthcare.fdamystudies.controller.UserProfileController;
@@ -13,12 +19,15 @@ import com.google.cloud.healthcare.fdamystudies.service.FdaEaUserDetailsServiceI
 import com.google.cloud.healthcare.fdamystudies.service.UserManagementProfileService;
 import com.google.cloud.healthcare.fdamystudies.testutils.Constants;
 import com.google.cloud.healthcare.fdamystudies.testutils.TestUtils;
+import com.jayway.jsonpath.JsonPath;
 
 public class UserProfileControllerTest extends BaseMockIT {
 
   private static final String PING_PATH = "/ping";
 
   private static final String USER_PROFILE_PATH = "/userProfile";
+
+  private static final String UPDATE_USER_PROFILE_PATH = "/updateUserProfile";
 
   @Autowired private UserProfileController profileController;
 
@@ -54,5 +63,20 @@ public class UserProfileControllerTest extends BaseMockIT {
     // Invalid userId
     headers.set(Constants.USER_ID_HEADER, Constants.INVALID_USER_ID);
     performGet(USER_PROFILE_PATH, headers, BAD_REQUEST);
+  }
+
+  @Test
+  public void updateUserProfileSuccess() throws Exception {
+    HttpHeaders headers = TestUtils.getCommonHeaders(Constants.USER_ID_HEADER);
+
+    SettingsRespBean settingRespBean = new SettingsRespBean(true, true, true, true, "", "");
+    UserRequestBean userRequestBean = new UserRequestBean(settingRespBean, new InfoBean());
+    String requestJson = getObjectMapper().writeValueAsString(userRequestBean);
+    performPost(
+        UPDATE_USER_PROFILE_PATH, requestJson, headers, String.valueOf(HttpStatus.OK.value()), OK);
+    MvcResult result = performGet(USER_PROFILE_PATH, headers, OK);
+    boolean remote =
+        JsonPath.read(result.getResponse().getContentAsString(), "$.settings.remoteNotifications");
+    assertTrue(remote);
   }
 }
