b69a2d800ca8a3fa496145299b00b2b5d4130a71
aswinijena100 <ashwinikumarj@boston-technology.com>
2020-07-17 13:01:58 +0530

    added deactivate api integration test

--- user-registration-server-ws/user-mgmt/src/test/java/com/google/cloud/healthcare/fdamystudies/controller/tests/UserProfileControllerTest.java
+++ user-registration-server-ws/user-mgmt/src/test/java/com/google/cloud/healthcare/fdamystudies/controller/tests/UserProfileControllerTest.java
@@ -1,20 +1,31 @@
 package com.google.cloud.healthcare.fdamystudies.controller.tests;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.verify;
 import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.web.servlet.MvcResult;
 
+import com.google.cloud.healthcare.fdamystudies.bean.StudyReqBean;
+import com.google.cloud.healthcare.fdamystudies.beans.DeactivateAcctBean;
 import com.google.cloud.healthcare.fdamystudies.beans.InfoBean;
 import com.google.cloud.healthcare.fdamystudies.beans.SettingsRespBean;
 import com.google.cloud.healthcare.fdamystudies.beans.UserRequestBean;
 import com.google.cloud.healthcare.fdamystudies.common.BaseMockIT;
 import com.google.cloud.healthcare.fdamystudies.config.AppConfig;
 import com.google.cloud.healthcare.fdamystudies.controller.UserProfileController;
+import com.google.cloud.healthcare.fdamystudies.model.UserDetailsBO;
 import com.google.cloud.healthcare.fdamystudies.service.FdaEaUserDetailsServiceImpl;
 import com.google.cloud.healthcare.fdamystudies.service.UserManagementProfileService;
 import com.google.cloud.healthcare.fdamystudies.testutils.Constants;
@@ -29,6 +40,8 @@ public class UserProfileControllerTest extends BaseMockIT {
 
   private static final String UPDATE_USER_PROFILE_PATH = "/updateUserProfile";
 
+  private static final String DEACTIVATE_PATH = "/deactivate";
+
   @Autowired private UserProfileController profileController;
 
   @Autowired private UserManagementProfileService profileService;
@@ -36,6 +49,12 @@ public class UserProfileControllerTest extends BaseMockIT {
   @Autowired private FdaEaUserDetailsServiceImpl service;
 
   @Autowired private AppConfig appconfig;
+
+  @Value("${auth.server.deactivateUrl}")
+  private String deactivateUrl;
+
+  @Value("${response.server.url.participant.withdraw}")
+  private String withdrawUrl;
 
   @Test
   public void contextLoads() {
@@ -78,5 +97,38 @@ public class UserProfileControllerTest extends BaseMockIT {
     boolean remote =
         JsonPath.read(result.getResponse().getContentAsString(), "$.settings.remoteNotifications");
     assertTrue(remote);
+  }
+
+  @Test
+  public void deactivateAccountSuccess() throws Exception {
+    HttpHeaders headers = TestUtils.getCommonHeaders(Constants.USER_ID_HEADER);
+
+    StudyReqBean studyReqBean = new StudyReqBean(Constants.STUDY_ID, Constants.DELETE);
+    List<StudyReqBean> list = new ArrayList<StudyReqBean>();
+    list.add(studyReqBean);
+    DeactivateAcctBean acctBean = new DeactivateAcctBean(list);
+    String requestJson = getObjectMapper().writeValueAsString(acctBean);
+    performDelete(DEACTIVATE_PATH, requestJson, headers, Constants.SUCCESS, OK);
+
+    UserDetailsBO daoResp = service.loadUserDetailsByUserId(Constants.VALID_USER_ID);
+    assertEquals(3, daoResp.getStatus());
+
+    verify(1, postRequestedFor(urlEqualTo("/AuthServer/deactivate")));
+    verify(
+        1,
+        postRequestedFor(
+            urlEqualTo(
+                "/mystudies-response-server/participant/withdraw?studyId=studyId1&participantId=1&deleteResponses=delete")));
+  }
+
+  @Test
+  public void deactivateAccountBadRequest() throws Exception {
+    HttpHeaders headers = TestUtils.getCommonHeaders(Constants.USER_ID_HEADER);
+
+    // invalid userId
+    headers.set(Constants.USER_ID_HEADER, Constants.INVALID_USER_ID);
+    DeactivateAcctBean acctBean = new DeactivateAcctBean();
+    String requestJson = getObjectMapper().writeValueAsString(acctBean);
+    performDelete(DEACTIVATE_PATH, requestJson, headers, "", BAD_REQUEST);
   }
 }
