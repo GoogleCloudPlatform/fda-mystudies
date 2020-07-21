c0b0259c0c2387e77cfef96f13ad8688d01aea21
Scott Hardin <shardin@validcare.com>
2020-07-20 21:42:39 -0400

    Android Branding (#134)
    * android logo updates
    
    * update version
    
    * android logo updates
    
    * rebase to Prod-Release
    
    * android logo updates
    
    * update gitignore and remove iml files
    
    * update gitignore
    
    * android branding changes

--- Android/app/src/main/java/com/harvard/FdaApplication.java
+++ Android/app/src/main/java/com/harvard/FdaApplication.java
@@ -23,7 +23,8 @@ import android.content.Intent;
 import android.os.Build;
 import android.support.multidex.MultiDex;
 import com.crashlytics.android.Crashlytics;
-import com.facebook.stetho.Stetho;
+// Remove for release
+// import com.facebook.stetho.Stetho;
 import com.harvard.passcodemodule.PasscodeSetupActivity;
 import com.harvard.studyappmodule.StudyModuleSubscriber;
 import com.harvard.usermodule.UserModuleSubscriber;
@@ -31,7 +32,8 @@ import com.harvard.utils.AppController;
 import com.harvard.utils.AppVisibilityDetector;
 import com.harvard.utils.realm.RealmEncryptionHelper;
 import com.harvard.webservicemodule.WebserviceSubscriber;
-import com.uphyca.stetho_realm.RealmInspectorModulesProvider;
+// Remove for release
+// import com.uphyca.stetho_realm.RealmInspectorModulesProvider;
 import io.fabric.sdk.android.Fabric;
 import io.realm.Realm;
 
@@ -87,7 +89,8 @@ public class FdaApplication extends Application {
         RealmEncryptionHelper.initHelper(this, getString(R.string.app_name));
     byte[] key = realmEncryptionHelper.getEncryptKey();
 
-    //Remove for release builds
+    // Remove for release builds
+    /*
     Stetho.initialize(
         Stetho.newInitializerBuilder(this)
             .enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
@@ -97,6 +100,7 @@ public class FdaApplication extends Application {
                     .withDefaultEncryptionKey(key)
                     .build())
             .build());
+     */
   }
 
   private void startEventProcessing() {
