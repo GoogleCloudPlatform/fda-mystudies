ac80ea914fda97db82778d864f6e4df2302c13cc
firdoseb <firdoseb@boston-technology.com>
2020-07-27 21:05:21 +0530

    test cases issue has been fixed

--- UR-web-app/src/app/auth/callback/callback.component.spec.ts
+++ UR-web-app/src/app/auth/callback/callback.component.spec.ts
@@ -1,6 +1,9 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
-
 import {CallbackComponent} from './callback.component';
+import {RouterTestingModule} from '@angular/router/testing';
+import {AuthService} from 'src/app/service/auth.service';
+import {HttpClientModule} from '@angular/common/http';
+import {EntityService} from 'src/app/service/entity.service';
 
 describe('CallbackComponent', () => {
   let component: CallbackComponent;
@@ -9,6 +12,8 @@ describe('CallbackComponent', () => {
   beforeEach(async(async () => {
     await TestBed.configureTestingModule({
       declarations: [CallbackComponent],
+      imports: [RouterTestingModule, HttpClientModule],
+      providers: [AuthService, EntityService],
     }).compileComponents();
   }));
 
