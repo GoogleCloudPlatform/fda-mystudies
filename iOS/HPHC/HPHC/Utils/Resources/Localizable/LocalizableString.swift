3dd0837a84026f3dc82322e54853fd8ef978733d
Tushar Katyal <tushark@boston-technology.com>
2020-07-28 17:32:28 +0530

    Move hardcoded resource message to be localize.

--- iOS/HPHC/HPHC/Utils/Resources/Localizable/LocalizableString.swift
+++ iOS/HPHC/HPHC/Utils/Resources/Localizable/LocalizableString.swift
@@ -17,6 +17,7 @@ enum LocalizableString: String {
   case resourceNotAvailable = "This resource is currently unavailable."
   case aboutStudy = "About the Study"
   case leaveSubtitle = "This will also delete your app account."
+  case newResourceMessage = "New Resource Available"
 
   var localizedString: String { return NSLocalizedString(rawValue, comment: "") }
 }
