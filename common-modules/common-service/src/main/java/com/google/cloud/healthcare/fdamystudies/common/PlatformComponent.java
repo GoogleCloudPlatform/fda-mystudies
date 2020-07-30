8d227b85115edbbace70f2f248d244612436a035
Dhanya Kumar KV <dhanyak@boston-technology.com>
2020-07-30 15:55:39 +0530

    added a line spacing for readability
    added a line spacing for readability

--- common-modules/common-service/src/main/java/com/google/cloud/healthcare/fdamystudies/common/PlatformComponent.java
+++ common-modules/common-service/src/main/java/com/google/cloud/healthcare/fdamystudies/common/PlatformComponent.java
@@ -16,25 +16,33 @@ import lombok.Getter;
 public enum PlatformComponent {
   MOBILE_APPS(
       "MOBILE APPS", "The iOS and Android apps that participants use to interact with MyStudies"),
+
   RESPONSE_DATASTORE(
       "RESPONSE DATASTORE",
       "The back-end components that manage participant response data (does not include PII) "),
+
   PARTICIPANT_DATASTORE(
       "PARTICIPANT DATASTORE",
       "The back-end components that manage app user / study participant data (includes PII)"),
+
   STUDY_BUILDER("STUDY BUILDER", "The UI that researchers use to design and deploy studies"),
+
   STUDY_DATASTORE(
       "STUDY DATASTORE",
       "This is the back-end component that manages the study builder data and holds the study configurations"),
+
   SCIM_AUTH_SERVER(
       "SCIM AUTH SERVER",
       "This component implements login/consent flow and integrates with ORY Hydra for access and refresh tokens"),
+
   AUTH_SERVER(
       "Auth Server",
       "This is back-end component that manages user accounts and issues access and refresh tokens"),
+
   PARTICIPANT_MANAGER(
       "PARTICIPANT MANAGER",
       "The UI that administrators use to invite participants to studies and track enrollment progress"),
+
   CLOUD_STORAGE(
       "Cloud Storage", "for storing and accessing data on Google Cloud Platform infrastructure");
 
