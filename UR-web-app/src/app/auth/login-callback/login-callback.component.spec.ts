413d5c907ef2d174d2eaec0ddcc30caeb07b8bad
firdoseb <firdoseb@boston-technology.com>
2020-07-30 20:41:23 +0530

    test case issues has been fixed

--- UR-web-app/src/app/auth/login-callback/login-callback.component.spec.ts
+++ UR-web-app/src/app/auth/login-callback/login-callback.component.spec.ts
@@ -1,5 +1,8 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
-
+import {RouterTestingModule} from '@angular/router/testing';
+import {AuthService} from 'src/app/service/auth.service';
+import {HttpClientModule} from '@angular/common/http';
+import {EntityService} from 'src/app/service/entity.service';
 import {LoginCallbackComponent} from './login-callback.component';
 
 describe('LoginCallbackComponent', () => {
@@ -9,6 +12,8 @@ describe('LoginCallbackComponent', () => {
   beforeEach(async(async () => {
     await TestBed.configureTestingModule({
       declarations: [LoginCallbackComponent],
+      imports: [RouterTestingModule, HttpClientModule],
+      providers: [AuthService, EntityService],
     }).compileComponents();
   }));
 
--- UR-web-app/src/app/auth/login/login.component.spec.ts
+++ UR-web-app/src/app/auth/login/login.component.spec.ts
@@ -1,5 +1,8 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
-
+import {AuthService} from 'src/app/service/auth.service';
+import {HttpClientModule} from '@angular/common/http';
+import {EntityService} from 'src/app/service/entity.service';
+import {RouterTestingModule} from '@angular/router/testing';
 import {LoginComponent} from './login.component';
 
 describe('LoginComponent', () => {
@@ -9,6 +12,8 @@ describe('LoginComponent', () => {
   beforeEach(async(async () => {
     await TestBed.configureTestingModule({
       declarations: [LoginComponent],
+      providers: [AuthService, EntityService],
+      imports: [HttpClientModule, RouterTestingModule],
     }).compileComponents();
   }));
 
