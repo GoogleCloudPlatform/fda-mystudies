60892fb8be2d2d2ae86582b9bbd29f2644444e6a
Zohreh Jabbari <zjabbari@google.com>
2020-07-30 14:17:54 -0400

    Add fix for https URLs, change WCP return urls to https (#94)

--- iOS/HPHC/HPHC/Networking/Network setup/NetworkWebServiceHandler.swift
+++ iOS/HPHC/HPHC/Networking/Network setup/NetworkWebServiceHandler.swift
@@ -499,7 +499,7 @@ class NetworkWebServiceHandler: NSObject, URLSessionDelegate {
             )
             as? [String: Any]
           if let errorBody = responseDict {
-            error1 = self.configuration.parseError(errorResponse: errorBody)
+            error1 = self.configuration.parseError(errorResponse: errorBody )
           } else {
             error1 = error ?? NSError(domain: "", code: -101, userInfo: [:])
           }
@@ -547,6 +547,8 @@ class NetworkWebServiceHandler: NSObject, URLSessionDelegate {
         )
       }
       completionHandler(challengeDisposition, credential)
+    } else {
+      completionHandler(.performDefaultHandling, nil)
     }
   }
 }
