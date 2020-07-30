06a499880deac713d7495fa3f06b8030afbf72c7
Zohreh Jabbari <zjabbari@google.com>
2020-07-30 16:16:37 -0400

    Change more URLs to https + improve logs (#95)

--- WCP-WS/src/main/java/com/hphc/mystudies/web/servlet/RestAuthenticationFilter.java
+++ WCP-WS/src/main/java/com/hphc/mystudies/web/servlet/RestAuthenticationFilter.java
@@ -75,7 +75,8 @@ public class RestAuthenticationFilter implements Filter {
               httpServletRequest.getPathInfo())) {
         filter.doFilter(request, response);
       } else {
-        logger.warn("RestAuthenticationFilter - doFilter() - failed. authCredentials required");
+        logger.warn("RestAuthenticationFilter - doFilter() - failed for " +
+          httpServletRequest.getPathInfo() + ". authCredentials required");
         HttpServletResponse httpServletResponse = (HttpServletResponse) response;
         httpServletResponse.setStatus(HttpServletResponse.SC_BAD_REQUEST);
         httpServletResponse.setHeader("status", ErrorCodes.STATUS_102);
