d92cc15a44afb1d5d53be5764e23df0378ba8daa
navya-BTC <navyat@boston-technology.com>
2020-08-13 13:08:30 +0530

    Update CommonConstants.java

--- common-modules/common-service/src/main/java/com/google/cloud/healthcare/fdamystudies/common/CommonConstants.java
+++ common-modules/common-service/src/main/java/com/google/cloud/healthcare/fdamystudies/common/CommonConstants.java
@@ -19,6 +19,8 @@ public final class CommonConstants {
 
   public static final Integer ACTIVE_STATUS = 1;
 
+  public static final Integer INACTIVE_STATUS = 0;
+
   public static final String NOT_APPLICABLE = "NA";
 
   public static final String OPEN_STUDY = "OPEN";
