812eee78fb13ec5256442da1fa2616bfb29f7b0e
Xin <xingao267@users.noreply.github.com>
2020-08-14 17:06:45 -0400

    add deployment template and clean up legacy terraform files (#756)

--- scripts/copy_mobile_app_info_to_sql.sh
+++ scripts/copy_mobile_app_info_to_sql.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
-# Script to copy AppId and OrgId from gcloud secret to CloudSQL.
+# Script to copy AppId from gcloud secret to CloudSQL.
 set -e
 
-# TODO: Change these to env variables.
-SECRET_PROJECT=heroes-hat-dev-devops
-DATA_PROJECT=heroes-hat-dev-data
-SQL_IMPORT_BUCKET=heroes-hat-dev-data-sql-import
+PREFIX=example
+ENV=dev
+
+SECRET_PROJECT=${PREFIX}-${ENV}-secrets
+DATA_PROJECT=${PREFIX}-${ENV}-data
+SQL_IMPORT_BUCKET=${PREFIX}-${ENV}-my-studies-sql-import
 TMPFILE=$(mktemp)
 ORG_NAME="Test Org"
 
 # Write user registration server db name to TMPFILE.
 echo "USE \`mystudies_userregistration\`;" >> ${TMPFILE}
 
-# Read AppId and OrgId from secrets..
-APP_ID=`gcloud --project=${SECRET_PROJECT} secrets versions access latest --secret=mobile-app-appid`
-ORG_ID=`gcloud --project=${SECRET_PROJECT} secrets versions access latest --secret=mobile-app-orgid`
+# Read AppId from secrets.
+APP_ID=`gcloud --project=${SECRET_PROJECT} secrets versions access latest --secret=manual-mobile-app-appid`
 
 # Write corresponding SQL commands to TMPFILE.
-echo "INSERT INTO org_info (id, name, org_id) VALUES(1, \"${ORG_NAME}\", \"${ORG_ID}\");" >> ${TMPFILE}
 echo "INSERT INTO app_info (app_info_id, custom_app_id, org_info_id) VALUES(1, \"${APP_ID}\", 1);" >> ${TMPFILE}
 echo "INSERT INTO locations (id, is_default) VALUES(1, \"Y\");" >> ${TMPFILE}
 
