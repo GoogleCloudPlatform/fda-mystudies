5cc7c1f43d1949c2a31fab70cb96069bd527cf01
monica-BTC <monicag@boston-technology.com>
2020-08-14 12:36:50 +0530

    resolved conflict errors
    resolved conflict errors

--- participant-manager-module/participant-manager-service/src/main/java/com/google/cloud/healthcare/fdamystudies/controller/AppController.java
+++ participant-manager-module/participant-manager-service/src/main/java/com/google/cloud/healthcare/fdamystudies/controller/AppController.java
@@ -8,6 +8,7 @@
 
 package com.google.cloud.healthcare.fdamystudies.controller;
 
+import com.google.cloud.healthcare.fdamystudies.beans.AppParticipantsResponse;
 import com.google.cloud.healthcare.fdamystudies.beans.AppResponse;
 import com.google.cloud.healthcare.fdamystudies.common.ErrorCode;
 import com.google.cloud.healthcare.fdamystudies.service.AppService;
@@ -15,8 +16,6 @@ import java.util.Arrays;
 import java.util.Optional;
 import javax.servlet.http.HttpServletRequest;
 import org.apache.commons.lang3.ArrayUtils;
-import com.google.cloud.healthcare.fdamystudies.service.AppService;
-import javax.servlet.http.HttpServletRequest;
 import org.slf4j.ext.XLogger;
 import org.slf4j.ext.XLoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,9 +28,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.google.cloud.healthcare.fdamystudies.beans.AppParticipantsResponse;
-import com.google.cloud.healthcare.fdamystudies.beans.AppResponse;
-import com.google.cloud.healthcare.fdamystudies.service.AppService;
 import static com.google.cloud.healthcare.fdamystudies.common.CommonConstants.USER_ID_HEADER;
 
 @RestController
@@ -43,6 +39,8 @@ public class AppController {
   @Autowired private AppService appService;
 
   private static final String STATUS_LOG = "status=%d";
+
+  private static final String BEGIN_REQUEST_LOG = "%s request";
 
   @GetMapping
   public ResponseEntity<AppResponse> getApps(
