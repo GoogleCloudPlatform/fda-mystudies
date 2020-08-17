2fca83e4a8e64ede91d35e5623bc2c0a3f8dbc5d
Madhurya N <madhuryan@boston-technology.com>
2020-08-17 12:43:48 +0530

    Partcicpant activity state integration test cases

--- response-server-module/response-server-service/src/test/java/com/google/cloud/healthcare/fdamystudies/utils/Constants.java
+++ response-server-module/response-server-service/src/test/java/com/google/cloud/healthcare/fdamystudies/utils/Constants.java
@@ -24,4 +24,18 @@ public final class Constants {
   public static final String APPLICATION_ID_HEADER = "applicationId";
 
   public static final String STUDY_ID = "study id value";
+
+  public static final String ACCESS_TOKEN_HEADER = "accessToken";
+
+  public static final String CLIENT_TOKEN_HEADER = "clientToken";
+
+  public static final String USER_ID_HEADER = "userId";
+
+  public static final String ACCESS_TOKEN_VALUE = "access Token value";
+
+  public static final String CLIENT_TOKEN_VALUE = "client token value";
+
+  public static final String INVALID_CLIENT_ID_VALUE = "invalid clientId value";
+
+  public static final String VALID_USER_ID = "kR2g5m2pJPP0P31-WNFYK8Al7jBP0mJ-cTSFJJHJ4DewuCg";
 }
