308a15b0e42d54f84c025b2674b3b80bf0c9a607
Kantharaju T <kantharajut@boston-technology.com>
2020-08-18 09:43:19 +0530

    Fixed PR comments

--- user-registration-server-ws/enroll-mgmt-module/enroll-mgmt/src/test/java/com/google/cloud/healthcare/fdamystudies/testutils/TestUtils.java
+++ user-registration-server-ws/enroll-mgmt-module/enroll-mgmt/src/test/java/com/google/cloud/healthcare/fdamystudies/testutils/TestUtils.java
@@ -12,7 +12,6 @@ public class TestUtils {
     headers.add(Constants.ACCESS_TOKEN_HEADER, Constants.ACCESS_TOKEN_VALUE);
     headers.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON);
     headers.add(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON);
-    // headers.add("Authorization", "Bearer 7fd50c2c-d618-493c-89d6-f1887e3e4bb8");
     return headers;
   }
 }
