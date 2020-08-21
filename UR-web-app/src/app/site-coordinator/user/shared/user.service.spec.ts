baaf716668aad99dd12f36606d8f7f637e1d5c2b
firdoseb <firdoseb@boston-technology.com>
2020-08-21 20:48:16 +0530

    add admin has been implemented

--- UR-web-app/src/app/site-coordinator/user/shared/user.service.spec.ts
+++ UR-web-app/src/app/site-coordinator/user/shared/user.service.spec.ts
@@ -1,16 +1,77 @@
-import {TestBed} from '@angular/core/testing';
-
+import {TestBed, fakeAsync} from '@angular/core/testing';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
 import {UserService} from './user.service';
-
+import {NO_ERRORS_SCHEMA} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {
+  expectedAppDetails,
+  addUserResponse,
+  addUserRequest,
+} from 'src/app/entity/mock-app-details';
+import {of, throwError} from 'rxjs';
+import {ApiResponse} from 'src/app/entity/api.response.model';
 describe('UserService', () => {
   let service: UserService;
-
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      schemas: [NO_ERRORS_SCHEMA],
+    });
     service = TestBed.inject(UserService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should return apps list for the user creation', () => {
+    const httpServiceSpyObj = jasmine.createSpyObj<HttpClient>('HttpClient', {
+      get: of(expectedAppDetails),
+    });
+    service = new UserService(httpServiceSpyObj);
+
+    service
+      .getAllApps()
+      .subscribe(
+        (appDetails) =>
+          expect(appDetails).toEqual(
+            expectedAppDetails,
+            'expected App Details',
+          ),
+        fail,
+      );
+    expect(httpServiceSpyObj.get).toHaveBeenCalledTimes(1);
+  });
+
+  it('should post the expected new user data', () => {
+    const httpServiceSpyObj = jasmine.createSpyObj<HttpClient>('HttpClient', {
+      post: of(addUserResponse),
+    });
+    service = new UserService(httpServiceSpyObj);
+
+    service
+      .add(addUserRequest)
+      .subscribe(
+        (successResponse: ApiResponse) =>
+          expect(successResponse).toEqual(addUserResponse),
+        fail,
+      );
+    expect(httpServiceSpyObj.post).toHaveBeenCalledTimes(1);
+  });
+
+  it('should return an error when the server returns a error status code', fakeAsync(() => {
+    const errorResponse = {
+      message: 'User does not exist',
+    } as ApiResponse;
+    const httpServiceSpyObj = jasmine.createSpyObj<HttpClient>('HttpClient', {
+      get: throwError(errorResponse),
+    });
+    service = new UserService(httpServiceSpyObj);
+    service.getAllApps().subscribe(
+      () => fail('expected an error, not app details'),
+      (error: ApiResponse) => {
+        expect(error.message).toContain(errorResponse.message);
+      },
+    );
+  }));
 });
--- UR-web-app/src/app/site-coordinator/user/shared/user.service.ts
+++ UR-web-app/src/app/site-coordinator/user/shared/user.service.ts
@@ -1,6 +1,22 @@
 import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {AppDetails} from './app-details';
+import {Observable} from 'rxjs';
+import {environment} from '@environment';
+import {ApiResponse} from 'src/app/entity/api.response.model';
+import {User} from 'src/app/entity/user';
 
 @Injectable({
   providedIn: 'root',
 })
-export class UserService {}
+export class UserService {
+  constructor(private readonly http: HttpClient) {}
+  getAllApps(): Observable<AppDetails> {
+    return this.http.get<AppDetails>(
+      `${environment.baseUrl}/apps?fields=studies,sites`,
+    );
+  }
+  add(user: User): Observable<ApiResponse> {
+    return this.http.post<ApiResponse>(`${environment.baseUrl}/users`, user);
+  }
+}
