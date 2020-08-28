aa0a19f51f4ed2b5bbdcef2c8f4c5b1270e2aa4f
navya-BTC <navyat@boston-technology.com>
2020-08-28 22:21:15 +0530

    Response-server Token-introspect
    Response-server Token-introspect

--- response-server-module/response-server-service/src/main/java/com/google/cloud/healthcare/fdamystudies/service/CommonService.java
+++ response-server-module/response-server-service/src/main/java/com/google/cloud/healthcare/fdamystudies/service/CommonService.java
@@ -8,20 +8,12 @@
 
 package com.google.cloud.healthcare.fdamystudies.service;
 
-import com.google.cloud.healthcare.fdamystudies.exception.InvalidRequestException;
-import com.google.cloud.healthcare.fdamystudies.exception.SystemException;
-import com.google.cloud.healthcare.fdamystudies.exception.UnAuthorizedRequestException;
 import com.google.cloud.healthcare.fdamystudies.model.ActivityLog;
 import com.google.cloud.healthcare.fdamystudies.model.ParticipantBo;
 
 public interface CommonService {
-  Integer validateAccessToken(String userId, String accessToken, String clientToken);
 
   public ParticipantBo getParticipantInfoDetails(String participantId);
 
-  boolean validateServerClientCredentials(String clientId, String clientSecret)
-      throws SystemException, UnAuthorizedRequestException, InvalidRequestException;
-
-  public ActivityLog createActivityLog(
-      String userId, String activityName, String activtyDesc, String clientId);
+  public ActivityLog createActivityLog(String userId, String activityName, String activtyDesc);
 }
