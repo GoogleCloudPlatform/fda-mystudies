6e23f29d381a53894a8aa1df2e6e84a39924025e
rohithd <rohithd@boston-technology.com>
2020-09-04 21:57:39 +0530

    particpant-manager-webapp-study-sites-with-new -api-integration
    particpant-manager-webapp-study-sites-with-new -api-integration

--- UR-web-app/src/app/site-coordinator/studies/shared/study.model.ts
+++ UR-web-app/src/app/site-coordinator/studies/shared/study.model.ts
@@ -1,17 +1,26 @@
 import {Site} from './site.model';
 
 export interface Study {
-  appId: string;
-  appInfoId: number;
+  appId?: string;
+  appInfoId?: string;
   customId: string;
-  enrolledCount: number;
-  enrollmentPercentage: number;
-  id: number;
-  invitedCount: number;
+  enrollmentPercentage?: number;
+  id: string;
+  sitesCount: number;
   name: string;
   sites: Site[];
   studyPermission: number;
   totalSitesCount: number;
   type: string;
   logo: string;
+  invited?: number;
+  enrolled?: number;
+}
+
+export interface StudyResponse {
+  studies: Study[];
+  sitePermissionCount?: number;
+  status: number;
+  message: string;
+  code: string;
 }
