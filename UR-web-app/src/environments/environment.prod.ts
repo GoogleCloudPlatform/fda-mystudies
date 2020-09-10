1e216389232ed64f2283a40689fa87b8a0615d7e
RohithDC <rohithd@boston-technology.com>
2020-09-10 16:08:11 +0530

    SSl issue with URl

--- UR-web-app/src/environments/environment.prod.ts
+++ UR-web-app/src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
   baseUrl: 'http://35.193.185.224:8093/participant-manager-service',
   loginUrl: 'https://35.193.185.224:9000/oauth2/auth',
   authServerUrl: 'http://35.193.185.224:8087/oauth-scim-service',
-  redirectUrl: 'https://35.193.185.224:8087/oauth-scim-service/login',
+  redirectUrl: 'https://34.69.210.52/qa/oauth-scim-service/login',
   appId: 'PARTICIPANT-MANAGER',
   client_id: 'oauth-scim-client-id',
   appVersion: 'v0.1',
