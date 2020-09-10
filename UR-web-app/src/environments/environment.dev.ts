74ad7608f76432a4b23b9a987b43b92658d75aa4
RohithDC <rohithd@boston-technology.com>
2020-09-10 21:07:51 +0530

    PARTICIPANT MANAGER appid change and update

--- UR-web-app/src/environments/environment.dev.ts
+++ UR-web-app/src/environments/environment.dev.ts
@@ -5,7 +5,7 @@ export const environment = {
   loginUrl: 'https://35.193.185.224:9000/oauth2/auth',
   authServerUrl: 'http://35.193.185.224:8087/oauth-scim-service',
   redirectUrl: 'https://34.69.210.52/qa/oauth-scim-service/login',
-  appId: 'PARTICIPANT-MANAGER',
+  appId: 'PARTICIPANT MANAGER',
   client_id: 'oauth-scim-client-id',
   appVersion: 'v0.1',
   mobilePlatform: 'DESKTOP',
