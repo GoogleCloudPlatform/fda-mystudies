9f69461988a62abfd92c16f15f8dd2fd0914c778
firdoseb <firdoseb@boston-technology.com>
2020-09-17 14:46:33 +0530

    removed random string generation code

--- UR-web-app/src/app/service/auth.service.ts
+++ UR-web-app/src/app/service/auth.service.ts
@@ -113,14 +113,4 @@ export class AuthService {
     sessionStorage.clear();
     this.cookieService.deleteAll();
   }
-  private strRandom(length: number) {
-    let result = '';
-    const characters =
-      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    const charactersLength = characters.length;
-    for (let i = 0; i < length; i++) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result;
-  }
 }
