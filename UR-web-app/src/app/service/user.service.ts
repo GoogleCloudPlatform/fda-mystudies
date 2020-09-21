c97998d7ef5f4a8ac56d968576e875c687357514
firdoseb <firdoseb@boston-technology.com>
2020-09-21 21:12:16 +0530

    code review comments has been resolved

--- UR-web-app/src/app/service/user.service.ts
+++ UR-web-app/src/app/service/user.service.ts
@@ -6,8 +6,6 @@ import {environment} from 'src/environments/environment';
 import {Profile} from '../site-coordinator/account/shared/profile.model';
 @Injectable({providedIn: 'root'})
 export class UserService {
-  user = {} as Profile;
-
   constructor(private readonly http: HttpClient) {}
 
   getUserDetails(): Observable<User> {
@@ -16,7 +14,8 @@ export class UserService {
 
   getUserProfile(): Profile {
     const userObject = sessionStorage.getItem('user');
-    if (userObject) this.user = JSON.parse(userObject) as Profile;
-    return this.user;
+    let user = {} as Profile;
+    if (userObject) user = JSON.parse(userObject) as Profile;
+    return user;
   }
 }
