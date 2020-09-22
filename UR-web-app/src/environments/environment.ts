558ecff88ca7a085e50d8ae0a75195ce29ef65c8
firdoseb <firdoseb@boston-technology.com>
2020-09-22 17:44:12 +0530

    oauth server redirect_uri and clientId updated

--- UR-web-app/src/environments/environment.ts
+++ UR-web-app/src/environments/environment.ts
@@ -7,8 +7,8 @@ export const environment = {
   baseUrl: 'http://35.193.185.224:8093/participant-manager-service',
   loginUrl: 'https://35.193.185.224:9000/oauth2/auth',
   authServerUrl: 'http://35.193.185.224:8087/oauth-scim-service',
-  redirectUrl: 'https://34.69.210.52/qa/oauth-scim-service/login',
-  clientId: 'oauth-scim-client-id',
+  redirectUrl: 'https://34.69.210.52/qa/oauth-scim-service/callback',
+  clientId: 'MYSTUDIES_PARTICIPANT_MANAGER',
   appVersion: 'v0.1',
 };
 
