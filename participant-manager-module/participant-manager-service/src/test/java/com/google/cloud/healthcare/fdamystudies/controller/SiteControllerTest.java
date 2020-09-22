7602ea7bdf13c7c49e7f292eafb875ffe0fdc46c
monica-BTC <monicag@boston-technology.com>
2020-09-22 20:30:46 +0530

    Fixed Issue#959
    Fixed Issue#959

--- participant-manager-module/participant-manager-service/src/test/java/com/google/cloud/healthcare/fdamystudies/controller/SiteControllerTest.java
+++ participant-manager-module/participant-manager-service/src/test/java/com/google/cloud/healthcare/fdamystudies/controller/SiteControllerTest.java
@@ -934,8 +934,7 @@ public class SiteControllerTest extends BaseMockIT {
             jsonPath("$.message", is(MessageCode.GET_PARTICIPANT_DETAILS_SUCCESS.getMessage())));
     verifyTokenIntrospectRequest(2);
 
-    // get consent history for default page (0), limit (5) and sort by created timestamp in
-    // descending order
+    // get all consent history if page and limit values are null
     mockMvc
         .perform(
             get(
@@ -953,12 +952,10 @@ public class SiteControllerTest extends BaseMockIT {
         .andExpect(jsonPath("$.participantDetails.enrollments").isArray())
         .andExpect(jsonPath("$.participantDetails.enrollments", hasSize(1)))
         .andExpect(jsonPath("$.participantDetails.consentHistory").isArray())
-        .andExpect(jsonPath("$.participantDetails.consentHistory", hasSize(5)))
+        .andExpect(jsonPath("$.participantDetails.consentHistory", hasSize(21)))
         .andExpect(jsonPath("$.totalConsentHistoryCount", is(21)))
         .andExpect(
-            jsonPath(
-                "$.participantDetails.consentHistory[0].consentVersion",
-                is(CONSENT_VERSION + String.valueOf(20))))
+            jsonPath("$.participantDetails.consentHistory[0].consentVersion", is(CONSENT_VERSION)))
         .andExpect(
             jsonPath("$.message", is(MessageCode.GET_PARTICIPANT_DETAILS_SUCCESS.getMessage())));
     verifyTokenIntrospectRequest(3);
