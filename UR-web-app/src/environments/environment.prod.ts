53cde352e46b3ac49cd02967d473231e74cf7677
firdoseb <firdoseb@boston-technology.com>
2020-09-23 14:29:46 +0530

    proxy server conf for https calls

--- UR-web-app/src/environments/environment.prod.ts
+++ UR-web-app/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://35.193.185.224:8093/participant-manager-service',
+  baseUrl: '/api/participant-manager-service',
   loginUrl: 'https://35.193.185.224:9000/oauth2/auth',
-  authServerUrl: 'http://35.193.185.224:8087/oauth-scim-service',
+  authServerUrl: '/api/oauth-scim-service',
   redirectUrl: 'https://34.69.210.52/qa/oauth-scim-service/callback',
   clientId: 'MYSTUDIES_PARTICIPANT_MANAGER',
   appVersion: 'v0.1',
