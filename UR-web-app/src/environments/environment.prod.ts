b36af95ded0adc88656bae4470551cc3a23a1022
firdoseb <firdoseb@boston-technology.com>
2020-09-23 15:05:58 +0530

    reverted back reverse proxy configuration

--- UR-web-app/src/environments/environment.prod.ts
+++ UR-web-app/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  baseUrl: '/api/participant-manager-service',
+  baseUrl: 'http://35.193.185.224:8093/participant-manager-service',
   loginUrl: 'https://35.193.185.224:9000/oauth2/auth',
-  authServerUrl: '/api/oauth-scim-service',
+  authServerUrl: 'http://35.193.185.224:8087/oauth-scim-service',
   redirectUrl: 'https://34.69.210.52/qa/oauth-scim-service/callback',
   clientId: 'MYSTUDIES_PARTICIPANT_MANAGER',
   appVersion: 'v0.1',
