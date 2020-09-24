57cbb4c72fb59c64d221d46a17688b41014ef324
monica-BTC <monicag@boston-technology.com>
2020-09-24 12:25:11 +0530

    Update CommonConstants.java

--- common-modules/common-service/src/main/java/com/google/cloud/healthcare/fdamystudies/common/CommonConstants.java
+++ common-modules/common-service/src/main/java/com/google/cloud/healthcare/fdamystudies/common/CommonConstants.java
@@ -82,10 +82,8 @@ public final class CommonConstants {
   public static final String DELETE = "delete";
 
   public static final String RETAIN = "retain";
-  
-  public static final String NO_OF_RECORDS = "5";
 
-  public static final double DEFAULT_PERCENTAGE = 100.00;
+  public static final String NO_OF_RECORDS = "5";
 
   public static final String RUN_ID = "run_id";
 
