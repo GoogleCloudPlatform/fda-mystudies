496e51796013bbc84b8176aa807011418ea846a2
firdoseb <firdoseb@boston-technology.com>
2020-09-25 15:23:11 +0530

    made environment clientid changes

--- UR-web-app/src/environments/environment.ts
+++ UR-web-app/src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
   loginUrl: 'https://35.193.185.224:9000/oauth2/auth',
   authServerUrl: 'http://35.193.185.224:8087/oauth-scim-service',
   redirectUrl: 'https://34.69.210.52/qa/oauth-scim-service/callback',
-  clientId: 'MYSTUDIES_PARTICIPANT_MANAGER',
+  clientId: 'MYSTUDIES_OAUTH_CLIENT',
   appVersion: 'v0.1',
 };
 
