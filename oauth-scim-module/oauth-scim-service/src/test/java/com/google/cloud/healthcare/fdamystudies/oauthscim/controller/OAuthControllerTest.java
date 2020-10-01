7db1ecf80c588eb07afbb38e29c054bb80c7eea8
Zohreh Jabbari <zjabbari@google.com>
2020-10-01 12:46:36 -0400

    fix oauthControllerTest

--- oauth-scim-module/oauth-scim-service/src/test/java/com/google/cloud/healthcare/fdamystudies/oauthscim/controller/OAuthControllerTest.java
+++ oauth-scim-module/oauth-scim-service/src/test/java/com/google/cloud/healthcare/fdamystudies/oauthscim/controller/OAuthControllerTest.java
@@ -432,7 +432,7 @@ public class OAuthControllerTest extends BaseMockIT {
 
     verify(
         1,
-        postRequestedFor(urlEqualTo("/oauth2/revoke"))
+        postRequestedFor(urlEqualTo("/hydra/oauth2/revoke"))
             .withRequestBody(new ContainsPattern(VALID_TOKEN)));
   }
 
