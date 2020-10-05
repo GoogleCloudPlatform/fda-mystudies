2b44dd9973b31aa4d594001f374ae993ca227615
aswinijena100 <ashwinikumarj@boston-technology.com>
2020-10-05 21:42:30 +0530

    moved out insert script from the main schema db script
    moved out insert script from the main schema db script

--- user-registration-server-ws/sqlscript/mystudies_participant_datastore_db_script.sql
+++ user-registration-server-ws/sqlscript/mystudies_participant_datastore_db_script.sql
@@ -423,12 +423,6 @@ CREATE TABLE IF NOT EXISTS `user_institution` (
   CONSTRAINT `FKrwwx4csj1ske5o4m74lmusqbv` FOREIGN KEY (`user_details_id`) REFERENCES `user_details` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `locations` (`id`, `created_time`, `created_by`, `custom_id`, `description`, `is_default`, `updated_time`, `modified_by`, `name`, `status`) VALUES
-	('1', '2020-10-05 19:45:08', '1', 'location1', 'location1', 'Y', '2020-09-25 19:45:24', '1', 'Location', 1);
-
-INSERT INTO `ur_admin_user` (`id`, `created_time`, `created_by`, `email`, `email_changed`, `first_name`, `last_name`, `location_permission`, `phone_number`, `security_code`, `security_code_expire_date`, `status`, `super_admin`, `ur_admin_auth_id`) VALUES
-('c9d30d67-0477-4a8c-8490-0fa1e0300bd0', '2020-10-05 19:50:15', '1', 'superadmin@gmail.com', b'0', 'Super', 'Admin', 1, '9876543210', 'af92f34e0ae6fc3c707446797b7db5b4be78ec699899061a448f8bc18990d75f', '2022-09-25 20:06:43', 1, b'1', '96494ebc2ae5ac344437ec19bfc0b09267a876015b277e1f6e9bfc871f578508');
-
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
