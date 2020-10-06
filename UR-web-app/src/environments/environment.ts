5b2289391b318c6c3845cf8b8196b08efed5690d
firdoseb <firdoseb@boston-technology.com>
2020-10-06 13:15:14 +0530

    environment.ts changed

--- UR-web-app/src/environments/environment.ts
+++ UR-web-app/src/environments/environment.ts
@@ -4,10 +4,10 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'https://35.193.185.224:8093/participant-manager-service',
+  baseUrl: 'http://35.193.185.224:8093/participant-manager-service',
   loginUrl: 'https://35.193.185.224:9000/oauth2/auth',
-  authServerUrl: 'https://35.193.185.224:8087/oauth-scim-service',
-  redirectUrl: 'https://34.69.210.224:8087/oauth-scim-service/callback',
+  authServerUrl: 'http://35.193.185.224:8087/oauth-scim-service',
+  redirectUrl: 'https://34.69.210.52/qa/oauth-scim-service/callback',
   clientId: 'MYSTUDIES_OAUTH_CLIENT',
   appVersion: 'v0.1',
 };
