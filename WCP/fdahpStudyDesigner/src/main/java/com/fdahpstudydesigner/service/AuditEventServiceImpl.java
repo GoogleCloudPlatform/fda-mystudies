65385804d2991acc18ccb5dcf38a840837b07e0a
Sami Nguyen <saminguyen@google.com>
2020-10-08 12:10:16 -0400

    remove extra empty line

--- WCP/fdahpStudyDesigner/src/main/java/com/fdahpstudydesigner/service/AuditEventServiceImpl.java
+++ WCP/fdahpStudyDesigner/src/main/java/com/fdahpstudydesigner/service/AuditEventServiceImpl.java
@@ -19,7 +19,6 @@ import org.springframework.stereotype.Service;
 import java.util.Collections;
 import java.util.Map;
 
-
 @Service
 public class AuditEventServiceImpl implements AuditEventService {
 
