523951c567db8434a988e78391be2ef1dba5d22e
Sami Nguyen <saminguyen@google.com>
2020-10-08 12:05:41 -0400

    GCP Stackdriver on StudyBuilder

--- WCP/fdahpStudyDesigner/src/main/java/com/fdahpstudydesigner/service/AuditEventServiceImpl.java
+++ WCP/fdahpStudyDesigner/src/main/java/com/fdahpstudydesigner/service/AuditEventServiceImpl.java
@@ -10,15 +10,22 @@ package com.fdahpstudydesigner.service;
 
 import static com.fdahpstudydesigner.common.JsonUtils.getObjectMapper;
 
+import com.google.cloud.MonitoredResource;
+import com.google.cloud.logging.*;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fdahpstudydesigner.bean.AuditLogEventRequest;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Service;
+import java.util.Collections;
+import java.util.Map;
+
 
 @Service
 public class AuditEventServiceImpl implements AuditEventService {
 
   private static Logger logger = Logger.getLogger(AuditEventServiceImpl.class);
+
+  private static final String AUDIT_LOG_NAME = "application-audit-log";
 
   @Override
   public void postAuditLogEvent(AuditLogEventRequest auditRequest) {
@@ -26,9 +33,20 @@ public class AuditEventServiceImpl implements AuditEventService {
         String.format("begin postAuditLogEvent() for %s event", auditRequest.getEventCode()));
 
     JsonNode requestBody = getObjectMapper().convertValue(auditRequest, JsonNode.class);
+    Logging logging = LoggingOptions.getDefaultInstance().getService();
 
-    // TODO (#703) integration with GCP stackdriver. Please remove the requestBody from below logger
-    // statement during stackdriver integration as it may contain PII information.
-    logger.debug(String.format("audit request=%s", requestBody));
+    // The data to write to the log
+    Map<String, Object> jsonPayloadMap = getObjectMapper().convertValue(auditRequest, Map.class);
+
+    LogEntry entry = LogEntry.newBuilder(Payload.JsonPayload.of(jsonPayloadMap))
+            .setTimestamp(auditRequest.getOccured().getTime())
+            .setSeverity(Severity.INFO)
+            .setLogName(AUDIT_LOG_NAME)
+            .setResource(MonitoredResource.newBuilder("global").build())
+            .build();
+    // Writes the log entry asynchronously
+    logging.write(Collections.singleton(entry));
+
+    logger.debug(String.format("postAuditLogEvent() for %s event finished", auditRequest.getEventCode()));
   }
 }
