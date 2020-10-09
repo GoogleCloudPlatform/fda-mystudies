3ca20cf68b0c71fd9c746e196098e69a822d3df4
firdoseb <firdoseb@boston-technology.com>
2020-10-09 20:30:50 +0530

    changed prop

--- participant-manager/src/environments/environment.prod.ts
+++ participant-manager/src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 // Replace the example domain name with your deployed address.
 export const environment = {
   production: true,
-  baseUrl: 'https://fda-mystudies.domain.com/participant-manager-datastore',
-  loginUrl: 'https://fda-mystudies.domain.com/hydra/oauth2/auth',
-  authServerUrl: 'https://fda-mystudies.domain.com/oauth-scim-service',
-  redirectUrl: 'https://fda-mystudies.domain.com/oauth-scim-service/callback',
+  baseUrl: 'http://35.193.185.224:8093/participant-manager-service',
+  loginUrl: 'https://35.193.185.224:9000/oauth2/auth',
+  authServerUrl: 'http://35.193.185.224:8087/oauth-scim-service',
+  redirectUrl: 'https://34.69.210.52/qa/oauth-scim-service/callback',
   clientId: 'MYSTUDIES_OAUTH_CLIENT',
   appVersion: 'v0.1',
 };
