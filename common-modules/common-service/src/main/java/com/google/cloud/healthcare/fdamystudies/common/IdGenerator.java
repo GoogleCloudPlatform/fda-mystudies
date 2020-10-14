bfbb63571efb35e1603ac7512124728ec7af99cc
Dhanya Kumar KV <63093033+dhanyak-btc@users.noreply.github.com>
2020-10-14 16:57:25 +0530

    Simplified ID generation, removed adding milliseconds to uuid.
    Simplified ID generation, removed adding milliseconds to uuid.

--- common-modules/common-service/src/main/java/com/google/cloud/healthcare/fdamystudies/common/IdGenerator.java
+++ common-modules/common-service/src/main/java/com/google/cloud/healthcare/fdamystudies/common/IdGenerator.java
@@ -8,33 +8,27 @@
 
 package com.google.cloud.healthcare.fdamystudies.common;
 
-import java.security.SecureRandom;
 import java.util.UUID;
 import org.apache.commons.lang3.RandomStringUtils;
 
 public final class IdGenerator {
 
-  private static SecureRandom secureRandom = new SecureRandom();
-
   private IdGenerator() {}
 
   /** Generates a random Id using UUID and current time (milliseconds). */
   public static String id() {
-    String millis = String.valueOf(System.currentTimeMillis());
     String uuid = UUID.randomUUID().toString();
     StringBuilder builder = new StringBuilder();
+
+    // replace hyphen (-) with random alpha numeric letter
     for (int i = 0; i < uuid.length(); i++) {
       if (uuid.charAt(i) == '-') {
-        builder.append(RandomStringUtils.randomAlphabetic(1));
+        builder.append(RandomStringUtils.randomAlphanumeric(1));
       } else {
         builder.append(uuid.charAt(i));
       }
     }
 
-    for (int i = 0; i < millis.length(); i++) {
-      int index = secureRandom.nextInt(builder.length() - 1);
-      builder.insert(index, millis.charAt(i));
-    }
-    return builder.toString();
+    return builder.toString().toLowerCase();
   }
 }
