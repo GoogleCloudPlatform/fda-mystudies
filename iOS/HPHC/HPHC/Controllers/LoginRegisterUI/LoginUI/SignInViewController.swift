e5f8b6017dd8684aee6507e878350ef837ef075d
Tushar Katyal <44088109+tushar-boston@users.noreply.github.com>
2020-10-15 11:22:25 +0530

    Fix Login callback navigation (#1368)
    (cherry picked from commit 64cee6cc2acb31e0e4d4e2aa596398a34320807f)

--- iOS/HPHC/HPHC/Controllers/LoginRegisterUI/LoginUI/SignInViewController.swift
+++ iOS/HPHC/HPHC/Controllers/LoginRegisterUI/LoginUI/SignInViewController.swift
@@ -77,10 +77,7 @@ class SignInViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     setupNavigation()
-    setupProgressView()
-    setupEstimatedProgressObserver()
     DispatchQueue.main.async {
-      self.webKitView.navigationDelegate = self
       self.load()
       self.initializeTermsAndPolicy()
     }
@@ -90,7 +87,9 @@ class SignInViewController: UIViewController {
     super.viewWillAppear(animated)
     // unhide navigationbar
     self.navigationController?.setNavigationBarHidden(false, animated: true)
-
+    self.webKitView.navigationDelegate = self
+    setupProgressView()
+    setupEstimatedProgressObserver()
     if viewLoadFrom != .signUp {
       User.resetCurrentUser()
     }
