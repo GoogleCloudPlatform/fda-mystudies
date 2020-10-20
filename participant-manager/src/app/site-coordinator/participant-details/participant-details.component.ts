fef6adb39342a7ac19d5b119a5d877690a511d63
firdoseb <firdoseb@boston-technology.com>
2020-10-20 14:07:01 +0530

    corrction of 1453 and 1482 fixed(class are adding but UI is not proper)

--- participant-manager/src/app/site-coordinator/participant-details/participant-details.component.ts
+++ participant-manager/src/app/site-coordinator/participant-details/participant-details.component.ts
@@ -6,7 +6,7 @@ import {ParticipantDetailsService} from './participant-details.service';
 import {ActivatedRoute} from '@angular/router';
 import {ToastrService} from 'ngx-toastr';
 import {getMessage} from 'src/app/shared/success.codes.enum';
-import {OnboardingStatus} from 'src/app/shared/enums';
+import {OnboardingStatus, StudyType} from 'src/app/shared/enums';
 import {ApiResponse} from 'src/app/entity/api.response.model';
 import {Location} from '@angular/common';
 @Component({
@@ -22,6 +22,7 @@ export class ParticipantDetailsComponent
   enableDisable = '';
   participant$: Observable<Participant> = of();
   onBoardingStatus = OnboardingStatus;
+  studyTypes = StudyType;
 
   constructor(
     private readonly locationLibrary: Location,
