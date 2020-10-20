2ed841a69fdec02280dcfb4d6a4f04ac3f69898a
monica-BTC <monicag@boston-technology.com>
2020-10-20 12:05:37 +0530

    Fixed issue #1241
    Fixed issue #1241

--- participant-manager-datastore/participant-manager-service/src/main/java/com/google/cloud/healthcare/fdamystudies/service/AppServiceImpl.java
+++ participant-manager-datastore/participant-manager-service/src/main/java/com/google/cloud/healthcare/fdamystudies/service/AppServiceImpl.java
@@ -174,7 +174,9 @@ public class AppServiceImpl implements AppService {
     List<AppDetails> appDetailsList = new ArrayList<>();
     for (AppEntity app : apps) {
       AppDetails appDetails = AppMapper.toAppDetails(app);
-      appDetails.setAppUsersCount(appUsersCountMap.get(app.getId()).getCount());
+      if (appUsersCountMap.containsKey(app.getId())) {
+        appDetails.setAppUsersCount(appUsersCountMap.get(app.getId()).getCount());
+      }
       appDetails.setStudiesCount(appStudiesCountMap.get(app.getId()).getCount());
       appDetails.setPermission(Permission.EDIT.value());
       Long enrolledCount = getCount(appEnrolledCountMap, app.getId());
