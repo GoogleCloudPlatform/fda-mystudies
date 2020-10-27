39304b0ba095d784d33a415c5451cea6b68d3ef0
navya-BTC <navyat@boston-technology.com>
2020-10-27 20:56:49 +0530

    Fixed issue #1654

--- participant-manager-datastore/participant-manager-service/src/main/java/com/google/cloud/healthcare/fdamystudies/mapper/StudyMapper.java
+++ participant-manager-datastore/participant-manager-service/src/main/java/com/google/cloud/healthcare/fdamystudies/mapper/StudyMapper.java
@@ -56,6 +56,7 @@ public final class StudyMapper {
       appStudyDetails.setCustomStudyId(study.getCustomId());
       appStudyDetails.setStudyName(study.getName());
       appStudyDetails.setStudyId(study.getId());
+      appStudyDetails.setStudyType(study.getType());
       List<AppSiteDetails> sites = SiteMapper.toParticipantSiteList(entry);
       appStudyDetails.setSites(sites);
     }
