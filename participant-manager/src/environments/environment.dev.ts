902743fe9b80974c9eef1d18310334d59c3b9680
firdoseb <firdoseb@boston-technology.com>
2020-11-17 20:11:02 +0530

    using dev env for deployment

--- participant-manager/src/environments/environment.dev.ts
+++ participant-manager/src/environments/environment.dev.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  baseUrl: ' http://35.222.67.4:8081/participant-manager-datastore',
+     baseUrl: ' http://35.222.67.4:8081/participant-manager-datastore',
   baseHref: '/',
-  loginUrl: 'https://35.193.185.224:9000/oauth2/auth',
+    loginUrl: 'https://35.193.185.224:9000/oauth2/auth',
   authServerUrl: ' http://35.222.67.4:8084/oauth-scim-service',
   redirectUrl: 'https://34.69.210.224:8087/oauth-scim-service/callback',
   clientId: 'MYSTUDIES_OAUTH_CLIENT',
