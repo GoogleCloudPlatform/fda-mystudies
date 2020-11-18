ad01b31d25ced4a07ef74eae9f40587eada50361
Dhanya Kumar KV <63093033+dhanyak-btc@users.noreply.github.com>
2020-11-18 19:59:52 +0530

    Disabled stackdriver implementation
    Disabled stackdriver implementation

--- common-modules/common-service/src/main/java/com/google/cloud/healthcare/fdamystudies/service/AuditEventServiceImpl.java
+++ common-modules/common-service/src/main/java/com/google/cloud/healthcare/fdamystudies/service/AuditEventServiceImpl.java
@@ -8,17 +8,9 @@
 
 package com.google.cloud.healthcare.fdamystudies.service;
 
-import static com.google.cloud.healthcare.fdamystudies.common.JsonUtils.getObjectMapper;
-
-import com.google.cloud.MonitoredResource;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.google.cloud.healthcare.fdamystudies.beans.AuditLogEventRequest;
-import com.google.cloud.logging.LogEntry;
-import com.google.cloud.logging.Logging;
-import com.google.cloud.logging.LoggingOptions;
-import com.google.cloud.logging.Payload;
-import com.google.cloud.logging.Severity;
-import java.util.Collections;
-import java.util.Map;
+import com.google.cloud.healthcare.fdamystudies.common.JsonUtils;
 import org.slf4j.ext.XLogger;
 import org.slf4j.ext.XLoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -39,23 +31,32 @@ public class AuditEventServiceImpl implements AuditEventService {
   public void postAuditLogEvent(AuditLogEventRequest auditRequest) {
     logger.entry(
         String.format("begin postAuditLogEvent() for %s event", auditRequest.getEventCode()));
+    try {
+      String auditRequestJson = JsonUtils.asJsonString(auditRequest);
+      logger.info(
+          String.format("%s event details %s", auditRequest.getEventCode(), auditRequestJson));
+    } catch (JsonProcessingException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    //   Logging logging = LoggingOptions.getDefaultInstance().getService();
 
-    Logging logging = LoggingOptions.getDefaultInstance().getService();
-
-    // The data to write to the log
-    Map<String, Object> jsonPayloadMap = getObjectMapper().convertValue(auditRequest, Map.class);
-
-    LogEntry entry =
-        LogEntry.newBuilder(Payload.JsonPayload.of(jsonPayloadMap))
-            .setTimestamp(auditRequest.getOccurred().getTime())
-            .setSeverity(Severity.INFO)
-            .setLogName(AUDIT_LOG_NAME)
-            .setResource(MonitoredResource.newBuilder("global").build())
-            .build();
-
-    // Writes the log entry asynchronously
-    logging.write(Collections.singleton(entry));
-    logger.exit(
-        String.format("postAuditLogEvent() for %s event finished", auditRequest.getEventCode()));
+    //    // The data to write to the log
+    //    Map<String, Object> jsonPayloadMap = getObjectMapper().convertValue(auditRequest,
+    // Map.class);
+    //
+    //    LogEntry entry =
+    //        LogEntry.newBuilder(Payload.JsonPayload.of(jsonPayloadMap))
+    //            .setTimestamp(auditRequest.getOccurred().getTime())
+    //            .setSeverity(Severity.INFO)
+    //            .setLogName(AUDIT_LOG_NAME)
+    //            .setResource(MonitoredResource.newBuilder("global").build())
+    //            .build();
+    //
+    //    // Writes the log entry asynchronously
+    //    logging.write(Collections.singleton(entry));
+    //    logger.exit(
+    //        String.format("postAuditLogEvent() for %s event finished",
+    // auditRequest.getEventCode()));
   }
 }
