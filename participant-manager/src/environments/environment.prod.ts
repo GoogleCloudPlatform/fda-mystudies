ffe25143c50b573d3394425acc71fb301b43bde5
aswinijena100 <ashwinikumarj@boston-technology.com>
2020-11-24 18:23:29 +0530

    Review comment issue fixes
    Review comment issue fixes

--- participant-manager/src/environments/environment.prod.ts
+++ participant-manager/src/environments/environment.prod.ts
@@ -4,8 +4,8 @@ export const environment = {
   baseUrl: 'https://fda-mystudies.domain.com/participant-manager-datastore',
   baseHref: '/participant-manager/',
   loginUrl: 'https://fda-mystudies.domain.com/oauth2/auth',
-  authServerUrl: 'https://fda-mystudies.domain.com/oauth-scim-service',
-  redirectUrl: 'https://fda-mystudies.domain.com/oauth-scim-service/callback',
+  authServerUrl: 'https://fda-mystudies.domain.com/auth-server',
+  redirectUrl: 'https://fda-mystudies.domain.com/auth-server/callback',
   clientId: 'MYSTUDIES_OAUTH_CLIENT',
   appVersion: 'v0.1',
 };
