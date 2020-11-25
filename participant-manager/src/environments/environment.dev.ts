3fee40650b4577241277304aca97ec2ac1c2d7fb
aswinijena100 <ashwinikumarj@boston-technology.com>
2020-11-25 15:17:35 +0530

    changed callback url
    changed callback url

--- participant-manager/src/environments/environment.dev.ts
+++ participant-manager/src/environments/environment.dev.ts
@@ -4,7 +4,7 @@ export const environment = {
   baseHref: '/',
   loginUrl: 'https://35.222.67.4:9000/oauth2/auth',
   authServerUrl: 'http://35.222.67.4:8084/auth-server',
-  redirectUrl: 'https://34.69.210.52/dev/oauth-scim-service/callback',
+  redirectUrl: 'https://34.69.210.52/dev/auth-server/callback',
   clientId: 'MYSTUDIES_OAUTH_CLIENT',
   appVersion: 'v0.1',
 };
