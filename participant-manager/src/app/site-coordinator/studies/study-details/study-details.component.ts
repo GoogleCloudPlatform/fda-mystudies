49dbf872d6fe69cdca7235dcecab4b6f0c7a9311
firdoseb <firdoseb@boston-technology.com>
2020-12-09 18:27:53 +0530

    formtting

--- participant-manager/src/app/site-coordinator/studies/study-details/study-details.component.ts
+++ participant-manager/src/app/site-coordinator/studies/study-details/study-details.component.ts
@@ -21,8 +21,7 @@ import {Location} from '@angular/common';
   templateUrl: './study-details.component.html',
   styleUrls: ['./study-details.component.scss'],
 })
-export class StudyDetailsComponent
-  extends UnsubscribeOnDestroyAdapter
+export class StudyDetailsComponent extends UnsubscribeOnDestroyAdapter
   implements OnInit {
   studyId = '';
   query$ = new BehaviorSubject('');
