656687b610849abb934c35afd12ea710e28e0073
prakashm181 <prakashm@boston-technology.com>
2020-12-11 21:29:13 +0530

    #1132-fix
    #1132-fix

--- participant-manager/src/app/service/display-header.service.ts
+++ participant-manager/src/app/service/display-header.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from '@angular/core';
 import {BehaviorSubject, Observable} from 'rxjs';
 @Injectable({providedIn: 'root'})
 export class DisplayHeaderService {
-  private readonly showHeader = new BehaviorSubject<boolean>(true);
+  private readonly showHeader = new BehaviorSubject<boolean>(false);
   showHeaders$: Observable<boolean>;
   constructor() {
     this.showHeaders$ = this.showHeader.asObservable();
