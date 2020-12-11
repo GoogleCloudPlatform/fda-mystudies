f498d73856f883584f07992f1e5978a291a98ed5
Kantharaju T <kantharajut@boston-technology.com>
2020-12-11 18:43:31 +0530

    fixed build issue
    fixed build issue

--- common-modules/common-service/src/main/java/com/google/cloud/healthcare/fdamystudies/common/ErrorCode.java
+++ common-modules/common-service/src/main/java/com/google/cloud/healthcare/fdamystudies/common/ErrorCode.java
@@ -327,14 +327,7 @@ public enum ErrorCode {
       HttpStatus.NOT_FOUND.toString(),
       "This view displays app-wise enrollment if you manage multiple studies."),
 
-
   USER_EMAIL_EXIST(400, "EC_0064", Constants.BAD_REQUEST, "The email already exists"),
-  
-  UNSUPPORTED_SORTBY_VALUE(
-      400, "EC_0076", HttpStatus.BAD_REQUEST.toString(), "Invalid sort by value"),
-
-  UNSUPPORTED_SORT_DIRECTION_VALUE(
-      400, "EC_0077", HttpStatus.BAD_REQUEST.toString(), "Invalid sorting direction"),
 
   REGISTRATION_EMAIL_SEND_FAILED(
       500,
@@ -353,7 +346,7 @@ public enum ErrorCode {
       "EC_0074",
       "Internal Server Error",
       "Sorry, an error occurred and your inquiry could not be sent to the organization. Please retry in some time."),
-  
+
   UNSUPPORTED_SORTBY_VALUE(
       400, "EC_0076", HttpStatus.BAD_REQUEST.toString(), "Invalid sortBy value"),
 
