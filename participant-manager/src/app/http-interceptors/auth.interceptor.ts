84492c0c8091ac42c50f7f1f5eee9da0c55013a6
prakashm181 <prakashm@boston-technology.com>
2020-12-14 11:01:37 +0530

    #2331 -issue-fix
    #2331 -issue-fix

--- participant-manager/src/app/http-interceptors/auth.interceptor.ts
+++ participant-manager/src/app/http-interceptors/auth.interceptor.ts
@@ -25,9 +25,9 @@ import {
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
   private isRefreshing = false;
-  private readonly refreshTokenSubject: BehaviorSubject<
-    unknown
-  > = new BehaviorSubject<unknown>(null);
+  private readonly refreshTokenSubject: BehaviorSubject<unknown> = new BehaviorSubject<unknown>(
+    null,
+  );
   constructor(
     private readonly spinner: NgxSpinnerService,
     private readonly toasterService: ToastrService,
@@ -137,7 +137,7 @@ export class AuthInterceptor implements HttpInterceptor {
             this.handle401Error(request, next);
           } else if (err.url === `${environment.authServerUrl}/oauth2/token`) {
             this.toasterService.error(
-              'Your session has expired and have been loged out successfully',
+              'Your session has expired. Please sign in again.',
             );
           } else if (err.error instanceof ErrorEvent) {
             this.toasterService.error(err.error.message);
