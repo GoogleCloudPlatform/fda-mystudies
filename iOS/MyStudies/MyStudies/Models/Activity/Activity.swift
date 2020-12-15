77beab41336d3c370be7e3d8b8b18ea822a484fe
Tushar Katyal <tushark@boston-technology.com>
2020-12-15 18:57:04 +0530

    Date format and text changes.

--- iOS/MyStudies/MyStudies/Models/Activity/Activity.swift
+++ iOS/MyStudies/MyStudies/Models/Activity/Activity.swift
@@ -160,6 +160,12 @@ class Activity {
 
   var schedulingType: ActivityScheduleType = .regular
 
+  /// A boolean indicates the activity start type.
+  var isLaunchWithStudy = false
+
+  /// A boolean indicates the activity end type.
+  var isStudyLifeTime = false
+
   /// Default Initializer
   init() {
 
@@ -202,6 +208,10 @@ class Activity {
 
     // Need to reCheck with actual dictionary when passed
     if Utilities.isValidObject(someObject: infoDict as AnyObject?) {
+
+      isLaunchWithStudy = infoDict["isLaunchStudy"] as? Bool ?? false
+
+      isStudyLifeTime = infoDict["isStudyLifeTime"] as? Bool ?? false
 
       if Utilities.isValidValue(someObject: infoDict[kActivityId] as AnyObject) {
         self.actvityId = (infoDict[kActivityId] as? String)!
