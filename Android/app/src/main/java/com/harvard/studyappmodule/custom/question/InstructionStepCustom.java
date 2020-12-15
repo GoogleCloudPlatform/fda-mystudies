f9e2fe33a07e56438d5dfd81da56e4763d572c88
Naveen <59356289+naveenr-btc@users.noreply.github.com>
2020-12-15 19:29:18 +0530

    formatting changes
    (cherry picked from commit c391c4f280cbe60b7d354389b946b4025abda3ca)

--- Android/app/src/main/java/com/harvard/studyappmodule/custom/question/InstructionStepCustom.java
+++ Android/app/src/main/java/com/harvard/studyappmodule/custom/question/InstructionStepCustom.java
@@ -2,18 +2,15 @@ package com.harvard.studyappmodule.custom.question;
 
 import org.researchstack.backbone.step.Step;
 
-public class InstructionStepCustom extends Step
-{
-    public InstructionStepCustom(String identifier, String title, String detailText)
-    {
-        super(identifier, title);
-        setText(detailText);
-        setOptional(false);
-    }
+public class InstructionStepCustom extends Step {
+  public InstructionStepCustom(String identifier, String title, String detailText) {
+    super(identifier, title);
+    setText(detailText);
+    setOptional(false);
+  }
 
-    @Override
-    public Class getStepLayoutClass()
-    {
-        return InstructionStepLayoutCustom.class;
-    }
+  @Override
+  public Class getStepLayoutClass() {
+    return InstructionStepLayoutCustom.class;
+  }
 }
