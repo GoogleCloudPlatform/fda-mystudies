7e0698714b51696b9a106fb527c6d41e6d783736
madhurya-btc <madhuryan@boston-technology.com>
2020-12-16 17:04:04 +0530

    Revert "Diryies context"
    This reverts commit 77bd86439abaa8df8b5536cd1eeb4ae77ad51c2c.

--- response-datastore/response-server-service/src/test/java/com/google/cloud/healthcare/fdamystudies/controller/ParticipantIdControllerTest.java
+++ response-datastore/response-server-service/src/test/java/com/google/cloud/healthcare/fdamystudies/controller/ParticipantIdControllerTest.java
@@ -40,12 +40,11 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MvcResult;
 
-// @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class ParticipantIdControllerTest extends BaseMockIT {
   @Autowired private ParticipantInfoRepository repository;
 
   @Test
-  @DirtiesContext(methodMode = DirtiesContext.MethodMode.BEFORE_METHOD)
   void shouldAddParticipant() throws Exception {
     // Step-1 call API to details to add participant id
     HttpHeaders headers = TestUtils.newCommonHeaders();
@@ -90,7 +89,6 @@ public class ParticipantIdControllerTest extends BaseMockIT {
   }
 
   @Test
-  @DirtiesContext(methodMode = DirtiesContext.MethodMode.BEFORE_METHOD)
   void shouldReturnBadRequestForMissingApplicationId() throws Exception {
     HttpHeaders headers = TestUtils.newCommonHeaders();
     mockMvc
@@ -105,7 +103,6 @@ public class ParticipantIdControllerTest extends BaseMockIT {
   }
 
   @Test
-  @DirtiesContext(methodMode = DirtiesContext.MethodMode.BEFORE_METHOD)
   void shouldReturnBadRequestForBlankStudyId() throws Exception {
 
     HttpHeaders headers = TestUtils.newCommonHeaders();
@@ -133,7 +130,6 @@ public class ParticipantIdControllerTest extends BaseMockIT {
   }
 
   @Test
-  @DirtiesContext(methodMode = DirtiesContext.MethodMode.BEFORE_METHOD)
   void shouldReturnBadRequestForBlankEnrollmentToken() throws Exception {
 
     HttpHeaders headers = TestUtils.newCommonHeaders();
