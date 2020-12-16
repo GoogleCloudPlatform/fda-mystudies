4d9ad39a8ebc49d6b88b8059d4b154b0752c47c7
firdoseb <firdoseb@boston-technology.com>
2020-12-16 19:16:19 +0530

    chanhged urls

--- participant-manager/src/environments/environment.dev.ts
+++ participant-manager/src/environments/environment.dev.ts
@@ -3,14 +3,13 @@ export const environment = {
   participantManagerDatastoreUrl:
     'https://35.193.185.224:8093/participant-manager-datastore',
   hydraLoginUrl: 'https://35.193.185.224:9000/oauth2/auth',
-  authServerUrl: 'https://35.193.185.224:8087/oauth-scim-service',
-  authServerRedirectUrl:
-    'https://34.69.210.224:8087/oauth-scim-service/callback',
+  authServerUrl: 'https://35.193.185.224:8087/auth-server',
+  authServerRedirectUrl: 'https://34.69.210.52/qa/auth-server/callback',
   hydraClientId: 'MYSTUDIES_OAUTH_CLIENT',
   appVersion: 'v0.1',
   termsPageTitle: 'terms title goes here',
   termsPageDescription: 'terms description goes here',
-   aboutPageTitle: 'about page title goes here',
+  aboutPageTitle: 'about page title goes here',
   aboutPageDescription: 'about page description goes here',
   copyright: 'Copyright 2020 Google LLC.',
 };
--- participant-manager/src/environments/environment.prod.ts
+++ participant-manager/src/environments/environment.prod.ts
@@ -4,8 +4,8 @@ export const environment = {
   participantManagerDatastoreUrl:
     'http://35.193.185.224:8093/participant-manager-service',
   hydraLoginUrl: 'https://35.193.185.224:9000/oauth2/auth',
-  authServerUrl: 'http://35.193.185.224:8087/oauth-scim-service',
-  authServerRedirectUrl: 'https://34.69.210.52/qa/oauth-scim-service/callback',
+  authServerUrl: 'http://35.193.185.224:8087/auth-server',
+  authServerRedirectUrl: 'https://34.69.210.52/qa/auth-server/callback',
   hydraClientId: 'MYSTUDIES_OAUTH_CLIENT',
   appVersion: 'v0.1',
   aboutPageTitle: 'about page title goes here',
