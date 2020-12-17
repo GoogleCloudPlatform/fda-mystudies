170afd50f4131e78f8d62e1ba702968b7654cde1
firdoseb <firdoseb@boston-technology.com>
2020-12-17 19:09:00 +0530

    issues #2166 #2425 #1270 fixed

--- participant-manager/src/environments/environment.ts
+++ participant-manager/src/environments/environment.ts
@@ -4,13 +4,12 @@
 
 export const environment = {
   production: false,
-  participantManagerDatastoreUrl:
-    'http://35.193.185.224:8093/participant-manager-service',
-  hydraLoginUrl: 'https://35.193.185.224:9000/oauth2/auth',
-  authServerUrl: 'http://35.193.185.224:8087/oauth-scim-service',
-  authServerRedirectUrl: 'https://34.69.210.52/qa/oauth-scim-service/callback',
-  hydraClientId: 'MYSTUDIES_OAUTH_CLIENT',
-  appVersion: 'v0.1',
+  participantManagerDatastoreUrl: '',
+  hydraLoginUrl: '',
+  authServerUrl: '',
+  authServerRedirectUrl: '',
+  hydraClientId: '',
+  appVersion: '',
   baseHref: '/',
   termsPageTitle: 'terms title goes here',
   termsPageDescription: 'terms description goes here',
