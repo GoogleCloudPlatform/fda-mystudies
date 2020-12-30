608e1c1f5ea9d42ea1ba555d402a8f1a8eae47cc
jkrogers18 <35972680+jkrogers18@users.noreply.github.com>
2020-12-30 07:34:18 -0500

    Update create_participant_manager_superadmin.sh

--- deployment/scripts/create_participant_manager_superadmin.sh
+++ deployment/scripts/create_participant_manager_superadmin.sh
@@ -28,6 +28,7 @@ SQL_IMPORT_BUCKET=${PREFIX}-${ENV}-mystudies-sql-import
 
 TMPFILE=$(mktemp)
 
+#Update first database
 echo "USE \`oauth_server_hydra\`;" >> ${TMPFILE}
 
 SALT=`printf "%s" uuidgen | iconv -t utf-8 | openssl dgst -sha512 | sed 's/^.* //'`
@@ -59,6 +60,12 @@ GCS_FILE=gs://${SQL_IMPORT_BUCKET}/participant_manager_superadmin.sql
 echo "Copying the sql file to ${GCS_FILE}"
 gsutil mv ${TMPFILE} ${GCS_FILE}
 
+# Import the GCS file to CloudSQL.
+echo "Importing ${GCS_FILE} to CloudSQL."
+gcloud sql import sql --project=${DATA_PROJECT} mystudies ${GCS_FILE}
+gsutil rm ${GCS_FILE}
+
+#Update second database
 echo "USE \`mystudies_participant_datastore\`;" >> ${TMPFILE}
 
 echo "Insert default location"
