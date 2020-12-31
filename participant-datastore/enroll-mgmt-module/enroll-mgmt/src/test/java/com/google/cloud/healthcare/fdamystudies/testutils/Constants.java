b90bec351758f58440d7e4e12b48da7142dfdccc
aswinijena100 <ashwinikumarj@boston-technology.com>
2020-12-31 15:37:23 +0530

    Issue fix for Release 2.0.1
    Issue fix for Release 2.0.1

--- participant-datastore/enroll-mgmt-module/enroll-mgmt/src/test/java/com/google/cloud/healthcare/fdamystudies/testutils/Constants.java
+++ participant-datastore/enroll-mgmt-module/enroll-mgmt/src/test/java/com/google/cloud/healthcare/fdamystudies/testutils/Constants.java
@@ -59,9 +59,9 @@ public final class Constants {
 
   public static final boolean BOOKMARKED = true;
 
-  public static final int COMPLETION = 0;
+  public static final int COMPLETION = 35;
 
-  public static final int ADHERENCE = 0;
+  public static final int ADHERENCE = 55;
 
   public static final boolean DELETE = true;
 }
