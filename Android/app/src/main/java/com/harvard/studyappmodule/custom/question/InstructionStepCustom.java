280e5f504e2093d51784a10903ae2d594cce4bf5
@bhushan91 <bhushanv@boston-technology.com>
2020-12-31 11:46:33 +0530

    Added javadoc comment for instructio step custom class

--- Android/app/src/main/java/com/harvard/studyappmodule/custom/question/InstructionStepCustom.java
+++ Android/app/src/main/java/com/harvard/studyappmodule/custom/question/InstructionStepCustom.java
@@ -3,6 +3,12 @@ package com.harvard.studyappmodule.custom.question;
 import org.researchstack.backbone.step.Step;
 
 public class InstructionStepCustom extends Step {
+  /**
+   * Customized instruction step that supports html content.
+   * @param identifier unique identifier to identify the step
+   * @param title title of the step
+   * @param detailText description of the step
+   */
   public InstructionStepCustom(String identifier, String title, String detailText) {
     super(identifier, title);
     setText(detailText);
