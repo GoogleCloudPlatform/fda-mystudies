657f97e7969e1c2c0ecca43b390290b5aeaf875b
@bhushan91 <bhushanv@boston-technology.com>
2020-12-31 12:14:10 +0530

    formatting

--- Android/app/src/main/java/com/harvard/studyappmodule/custom/question/InstructionStepCustom.java
+++ Android/app/src/main/java/com/harvard/studyappmodule/custom/question/InstructionStepCustom.java
@@ -5,6 +5,7 @@ import org.researchstack.backbone.step.Step;
 public class InstructionStepCustom extends Step {
   /**
    * Customized instruction step that supports html content.
+   *
    * @param identifier unique identifier to identify the step
    * @param title title of the step
    * @param detailText description of the step
