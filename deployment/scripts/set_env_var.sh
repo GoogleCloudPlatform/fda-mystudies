d4e60771a3c809bd0eda040c959c2615bcf0d985
Toshiaki HAMA <hama@solaro.jp>
2021-01-20 11:46:54 +0900

    Initial configuration of application properties for kyoto

--- deployment/scripts/set_env_var.sh
+++ deployment/scripts/set_env_var.sh
@@ -9,24 +9,27 @@
 
 # PREFIX is a name you choose for your deployment that will be prepended to various
 # directories, cloud resources and URLs (for example this could be `mystudies`)
-export PREFIX=btc
+export PREFIX=kyoto
 
 # ENV is a label you choose that will be appended to PREFIX in your directories
 # and cloud resources (for example this could be `dev`, `test` or `prod`)
-export ENV=terradeploy
+export ENV=demo
 
 # GIT_ROOT is the local path to the root of your cloned FDA MyStudies repository
-export GIT_ROOT=/home/yugandhare/fda-mystudies
+export GIT_ROOT=/Users/hama/Work/01.IT/01.project/Abby/01.アジア治験NW/02.repo_clone/fda-mystudies
 
 # LOCATION is the loctaion you specified for your deployment in
 # deployment.hcl, for example `us-central1`
-export LOCATION=us-east1
+export LOCATION=asia-northeast1
 
 # DOMAIN is the domain you will be using for your URLs (for example, 
 # `your_company_name.com` or `your_medical_center.edu`)
-export DOMAIN=boston-technology.com
+export DOMAIN=clipcrow.com
 
 # ENGINE_CONFIG and MYSTUDIES_TEMPLATE do not need to be changed unless
 # you have moved these files within your GitHub repo
 export ENGINE_CONFIG=${GIT_ROOT}/deployment/deployment.hcl
 export MYSTUDIES_TEMPLATE=${GIT_ROOT}/deployment/mystudies.hcl
+
+
+#export auth_server_client_id=QFMrv678quwk7Maf
