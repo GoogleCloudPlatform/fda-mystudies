6410c330e132e368fa6b54506453218d63d1ad35
madhurya-btc <madhuryan@boston-technology.com>
2021-01-20 16:03:47 +0530

    Issue #2947 fixed
    Issue #2947 fixed

--- study-builder/fdahpStudyDesigner/src/main/java/com/fdahpstudydesigner/dao/StudyQuestionnaireDAOImpl.java
+++ study-builder/fdahpStudyDesigner/src/main/java/com/fdahpstudydesigner/dao/StudyQuestionnaireDAOImpl.java
@@ -4261,6 +4261,11 @@ public class StudyQuestionnaireDAOImpl implements StudyQuestionnaireDAO {
                   }
                   if ((questionConditionBranchBo.getInputType() != null)
                       && questionConditionBranchBo.getInputType().equalsIgnoreCase("MF")) {
+                    questionConditionBranchBo.setInputTypeValue(
+                        questionConditionBranchBo
+                            .getInputTypeValue()
+                            .replace("&lt;", "<")
+                            .replace("&gt;", ">"));
                     session.save(questionConditionBranchBo);
                   }
                   if ((questionConditionBranchBo.getQuestionConditionBranchBos() != null)
@@ -4272,6 +4277,11 @@ public class StudyQuestionnaireDAOImpl implements StudyQuestionnaireDAO {
                         if (conditionBranchBo.getQuestionId() == null) {
                           conditionBranchBo.setQuestionId(questionsBo.getId());
                         }
+                        conditionBranchBo.setInputTypeValue(
+                            conditionBranchBo
+                                .getInputTypeValue()
+                                .replace("&lt;", "<")
+                                .replace("&gt;", ">"));
                         session.save(conditionBranchBo);
                       }
                     }
