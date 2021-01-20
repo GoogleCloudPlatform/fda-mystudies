9c16b39f24103215d082ad7ff84053aa01a06def
aswinijena100 <ashwinikumarj@boston-technology.com>
2021-01-20 11:51:45 +0530

    Resolved Conflicts
    Resolved Conflicts

--- common-modules/common-service/src/main/java/com/google/cloud/healthcare/fdamystudies/repository/ParticipantStudyRepository.java
+++ common-modules/common-service/src/main/java/com/google/cloud/healthcare/fdamystudies/repository/ParticipantStudyRepository.java
@@ -114,10 +114,10 @@ public interface ParticipantStudyRepository extends JpaRepository<ParticipantStu
 
   @Query(
       value =
-          "SELECT ps.site_id FROM participant_registry_site prs, participant_study_info ps, user_details ud "
-              + "WHERE prs.id=ps.participant_registry_site_id AND ud.user_id=:userId AND upper(prs.enrollment_token)=:token ",
+          "SELECT ps.id FROM participant_study_info ps, user_details ud "
+              + "WHERE ps.user_details_id=ud.id AND ps.study_info_id=:studyId AND ud.user_id=:userId ",
       nativeQuery = true)
-  public List<String> findByEmailAndStudyCustomIds(String email, List<String> studyCustomIds);
+  public List<String> findByStudyIdAndUserDetailId(String studyId, String userId);
 
   @Query(
       value =
