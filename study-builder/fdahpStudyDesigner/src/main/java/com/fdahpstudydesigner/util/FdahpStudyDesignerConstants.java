27d26baf386c23a08a981ef10a3aebc7da9a5a53
Kantharaju T <kantharajut@boston-technology.com>
2021-01-25 19:24:19 +0530

    Fixed issue #2997 #2989
    Fixed issue #2997 #2989

--- study-builder/fdahpStudyDesigner/src/main/java/com/fdahpstudydesigner/util/FdahpStudyDesignerConstants.java
+++ study-builder/fdahpStudyDesigner/src/main/java/com/fdahpstudydesigner/util/FdahpStudyDesignerConstants.java
@@ -67,7 +67,7 @@ public class FdahpStudyDesignerConstants {
       "The study must have at least one questionnaire or active task added before attempting this action.";
 
   public static final String ACTIVETASK_DATE_ERROR_MSG =
-      "One or more of the study’s activities or resources has a schedule that needs an anchor date. Please add an anchor date and try again.";
+      "One or more of the study's activities is scheduled for a date that has already expired. Please correct the same and try again.";
   public static final String ACTIVETASK_LIST = "activeTaskList";
   public static final String ACTIVITY_MESSAGE = "activityMsg";
   public static final String ACTIVITY_STUDY_ID = "activityStudyId";
