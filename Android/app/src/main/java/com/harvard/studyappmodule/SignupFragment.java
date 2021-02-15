0e050a094944b9c7dd6378a6cbebb91b3f651a72
Naveen <59356289+naveenr-btc@users.noreply.github.com>
2021-02-15 18:41:18 +0530

    review comment fixes

--- Android/app/src/main/java/com/harvard/studyappmodule/SignupFragment.java
+++ Android/app/src/main/java/com/harvard/studyappmodule/SignupFragment.java
@@ -1,6 +1,6 @@
 /*
  * Copyright © 2017-2019 Harvard Pilgrim Health Care Institute (HPHCI) and its Contributors.
- * Copyright 2020 Google LLC
+ * Copyright 2020-2021 Google LLC
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
  * associated documentation files (the "Software"), to deal in the Software without restriction, including
  * without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
@@ -11,6 +11,7 @@
  * Funding Source: Food and Drug Administration (“Funding Agency”) effective 18 September 2014 as Contract no. HHSF22320140030I/HHSF22301006T (the “Prime Contract”).
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
  */
 
 package com.harvard.studyappmodule;
@@ -84,8 +85,6 @@ public class SignupFragment extends Fragment implements ApiCall.OnAsyncRequestCo
   private String userAuth;
   private String userID;
   private RegistrationData registrationData;
-  String passwordPattern =
-          "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!\"#$%&'()*+,-.:;<=>?@\\[\\]^_`{|}~]).{8,64}$";
 
   @Override
   public void onAttach(Context context) {
@@ -246,7 +245,7 @@ public class SignupFragment extends Fragment implements ApiCall.OnAsyncRequestCo
       @Override
       public void onFocusChange(View v, boolean hasFocus) {
         if (!hasFocus) {
-          if (!password.getText().toString().matches(passwordPattern)) {
+          if (!password.getText().toString().matches(AppController.PASSWORD_PATTERN)) {
             password.setError(getResources().getString(R.string.password_validation));
           }
         }
@@ -271,7 +270,7 @@ public class SignupFragment extends Fragment implements ApiCall.OnAsyncRequestCo
     } else if (password.getText().toString().isEmpty()) {
       Toast.makeText(context, getResources().getString(R.string.password_empty), Toast.LENGTH_SHORT)
           .show();
-    } else if (!password.getText().toString().matches(passwordPattern)) {
+    } else if (!password.getText().toString().matches(AppController.PASSWORD_PATTERN)) {
       password.setError(getResources().getString(R.string.password_validation));
     } else if (checkPasswordContainsEmailID(
         email.getText().toString(), password.getText().toString())) {
