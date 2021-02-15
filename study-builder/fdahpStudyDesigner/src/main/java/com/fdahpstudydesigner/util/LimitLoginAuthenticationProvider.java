2810ace6bc64db4a5746534f8a88aab22370aca5
harisboston <harish@boston-technology.com>
2021-02-15 20:09:09 +0530

    Updated Copyright 2020-2021 Google LLC for SB, oauth, and CM
    Updated Copyright 2020-2021 Google LLC for SB, oauth, and Common M.

--- study-builder/fdahpStudyDesigner/src/main/java/com/fdahpstudydesigner/util/LimitLoginAuthenticationProvider.java
+++ study-builder/fdahpStudyDesigner/src/main/java/com/fdahpstudydesigner/util/LimitLoginAuthenticationProvider.java
@@ -1,6 +1,7 @@
 /*
  * Copyright © 2017-2018 Harvard Pilgrim Health Care Institute (HPHCI) and its Contributors.
- * Copyright 2020 Google LLC Permission is hereby granted, free of charge, to any person obtaining a
+ * Copyright 2020-2021 Google LLC
+ * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"), to deal in the
  * Software without restriction, including without limitation the rights to use, copy, modify,
  * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
--- study-builder/fdahpStudyDesigner/src/test/java/com/fdahpstudydesigner/controller/DashBoardAndProfileControllerTest.java
+++ study-builder/fdahpStudyDesigner/src/test/java/com/fdahpstudydesigner/controller/DashBoardAndProfileControllerTest.java
@@ -5,6 +5,7 @@
  * license that can be found in the LICENSE file or at
  * https://opensource.org/licenses/MIT.
  */
+
 package com.fdahpstudydesigner.controller;
 
 import static com.fdahpstudydesigner.common.StudyBuilderAuditEvent.ACCOUNT_DETAILS_VIEWED;
