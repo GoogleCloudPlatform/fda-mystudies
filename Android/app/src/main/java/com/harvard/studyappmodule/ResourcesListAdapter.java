9ddbffd6b37761ac53b558cac50b7d22cdef9ef1
Naveen <59356289+naveenr-btc@users.noreply.github.com>
2021-02-17 12:24:08 +0530

    button text changes for #3148
    (cherry picked from commit 59afa5ff07105540e87487b04a281555411f9e31)

--- Android/app/src/main/java/com/harvard/studyappmodule/ResourcesListAdapter.java
+++ Android/app/src/main/java/com/harvard/studyappmodule/ResourcesListAdapter.java
@@ -171,7 +171,7 @@ public class ResourcesListAdapter extends RecyclerView.Adapter<ResourcesListAdap
                 builder.setTitle(context.getResources().getString(R.string.leave_study) + "?");
                 builder.setMessage(message);
                 builder.setPositiveButton(
-                    context.getResources().getString(R.string.proceed_caps),
+                    context.getResources().getString(R.string.yes),
                     new DialogInterface.OnClickListener() {
                       @Override
                       public void onClick(DialogInterface dialog, int which) {
@@ -180,7 +180,7 @@ public class ResourcesListAdapter extends RecyclerView.Adapter<ResourcesListAdap
                     });
 
                 builder.setNegativeButton(
-                    context.getResources().getString(R.string.cancel_caps),
+                    context.getResources().getString(R.string.cancel),
                     new DialogInterface.OnClickListener() {
                       @Override
                       public void onClick(DialogInterface dialog, int which) {
