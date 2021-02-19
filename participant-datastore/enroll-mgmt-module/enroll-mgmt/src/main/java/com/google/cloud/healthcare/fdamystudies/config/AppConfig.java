1f1d5dba4b88c0805d44de8ead84ac45541339ac
Kantharaju T <kantharajut@boston-technology.com>
2021-02-19 17:28:52 +0530

    Fixed issue #3124
    Fixed issue #3124

--- participant-datastore/enroll-mgmt-module/enroll-mgmt/src/main/java/com/google/cloud/healthcare/fdamystudies/config/AppConfig.java
+++ participant-datastore/enroll-mgmt-module/enroll-mgmt/src/main/java/com/google/cloud/healthcare/fdamystudies/config/AppConfig.java
@@ -8,7 +8,15 @@
 
 package com.google.cloud.healthcare.fdamystudies.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
-public class AppConfig {}
+public class AppConfig extends CommonModuleConfiguration {
+
+  @Bean
+  public BCryptPasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+}
