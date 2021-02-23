779d2d76b3bd0147d04fc1fe51d706937e98b6b0
Chiranjibi009 <chiranjibid@boston-technology.com>
2021-02-23 11:43:01 +0530

    comment fixed

--- participant-datastore/enroll-mgmt-module/enroll-mgmt/src/main/java/com/google/cloud/healthcare/fdamystudies/util/ErrorResponseUtil.java
+++ participant-datastore/enroll-mgmt-module/enroll-mgmt/src/main/java/com/google/cloud/healthcare/fdamystudies/util/ErrorResponseUtil.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Google LLC
+ * Copyright 2020-2021 Google LLC
  *
  * Use of this source code is governed by an MIT-style
  * license that can be found in the LICENSE file or at
@@ -113,7 +113,7 @@ public class ErrorResponseUtil {
     INVALID_TOKEN("Sorry, this token is invalid. Please enter a valid token to continue."),
     UNAUTHORIZED_CLIENT("Unauthorized client"),
     UNAUTHORIZED("Unauthorized"),
-    INTERNAL_SERER_ERROR("Internal Serer Error"),
+    INTERNAL_SERVER_ERROR("Internal Server Error"),
     TOKEN_REQUIRED("Token is required");
 
     private final String value;
