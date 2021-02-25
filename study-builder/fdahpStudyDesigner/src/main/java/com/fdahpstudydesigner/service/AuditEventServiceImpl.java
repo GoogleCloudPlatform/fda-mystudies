e9c7db89664b8082cabbf37a6e702e983ad99f55
aswinijena100 <ashwinikumarj@boston-technology.com>
2021-02-25 12:55:40 +0530

    Stack Driver logging client shut down issue fix
    Stack Driver logging client shut down issue #2058  fix

--- study-builder/fdahpStudyDesigner/src/main/java/com/fdahpstudydesigner/service/AuditEventServiceImpl.java
+++ study-builder/fdahpStudyDesigner/src/main/java/com/fdahpstudydesigner/service/AuditEventServiceImpl.java
@@ -34,22 +34,25 @@ public class AuditEventServiceImpl implements AuditEventService {
   public void postAuditLogEvent(AuditLogEventRequest auditRequest) {
     logger.debug(
         String.format("begin postAuditLogEvent() for %s event", auditRequest.getEventCode()));
+    try (Logging logging = LoggingOptions.getDefaultInstance().getService()) {
+      JsonNode requestBody = getObjectMapper().convertValue(auditRequest, JsonNode.class);
 
-    JsonNode requestBody = getObjectMapper().convertValue(auditRequest, JsonNode.class);
-    Logging logging = LoggingOptions.getDefaultInstance().getService();
+      // The data to write to the log
+      Map<String, Object> jsonPayloadMap = getObjectMapper().convertValue(auditRequest, Map.class);
 
-    // The data to write to the log
-    Map<String, Object> jsonPayloadMap = getObjectMapper().convertValue(auditRequest, Map.class);
+      LogEntry entry =
+          LogEntry.newBuilder(Payload.JsonPayload.of(jsonPayloadMap))
+              .setTimestamp(auditRequest.getOccurred().getTime())
+              .setSeverity(Severity.INFO)
+              .setLogName(AUDIT_LOG_NAME)
+              .setResource(MonitoredResource.newBuilder("global").build())
+              .build();
+      // Writes the log entry asynchronously
+      logging.write(Collections.singleton(entry));
 
-    LogEntry entry =
-        LogEntry.newBuilder(Payload.JsonPayload.of(jsonPayloadMap))
-            .setTimestamp(auditRequest.getOccurred().getTime())
-            .setSeverity(Severity.INFO)
-            .setLogName(AUDIT_LOG_NAME)
-            .setResource(MonitoredResource.newBuilder("global").build())
-            .build();
-    // Writes the log entry asynchronously
-    logging.write(Collections.singleton(entry));
+    } catch (Exception e) {
+      logger.error(String.format("%s failed with an exception", auditRequest.getEventCode()), e);
+    }
 
     logger.debug(
         String.format("postAuditLogEvent() for %s event finished", auditRequest.getEventCode()));
