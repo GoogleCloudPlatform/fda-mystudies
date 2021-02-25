a1a229fdf314c1754ac4989f5ca838e741f27515
madhurya-btc <madhuryan@boston-technology.com>
2021-02-25 16:48:01 +0530

    swagger -user mgmt

--- participant-datastore/user-mgmt-module/user-mgmt/src/main/java/com/google/cloud/healthcare/fdamystudies/controller/StudiesController.java
+++ participant-datastore/user-mgmt-module/user-mgmt/src/main/java/com/google/cloud/healthcare/fdamystudies/controller/StudiesController.java
@@ -19,6 +19,8 @@ import com.google.cloud.healthcare.fdamystudies.common.UserMgmntAuditHelper;
 import com.google.cloud.healthcare.fdamystudies.mapper.AuditEventMapper;
 import com.google.cloud.healthcare.fdamystudies.service.StudiesServices;
 import com.google.cloud.healthcare.fdamystudies.util.ErrorCode;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
@@ -33,6 +35,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(
+    tags = "Studies",
+    value = "Studies",
+    description = "Operations pertaining to Studies in user management service")
 @RestController
 @Validated
 @RequestMapping("/studies")
@@ -43,6 +49,7 @@ public class StudiesController {
 
   @Autowired private UserMgmntAuditHelper userMgmntAuditHelper;
 
+  @ApiOperation(value = "Add or update studymetadata")
   @PostMapping("/studymetadata")
   public ResponseEntity<?> addUpdateStudyMetadata(
       @Valid @RequestBody StudyMetadataBean studyMetadataBean, HttpServletRequest request) {
@@ -63,6 +70,7 @@ public class StudiesController {
     return new ResponseEntity<>(errorBean, HttpStatus.OK);
   }
 
+  @ApiOperation(value = "Send Notification")
   @PostMapping("/sendNotification")
   public ResponseEntity<?> SendNotification(
       @Valid @RequestBody NotificationForm notificationForm, HttpServletRequest request)
