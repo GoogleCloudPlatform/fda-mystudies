ce8b67a81ad2b171b4d0b1810979e6af37be39b7
Kantharaju T <kantharajut@boston-technology.com>
2021-02-26 15:49:16 +0530

    Update V2_0_3__release.sql

--- db-migration/study-builder-db-migration/db/migration/V2_0_3__release.sql
+++ db-migration/study-builder-db-migration/db/migration/V2_0_3__release.sql
@@ -1,5 +1,10 @@
-UPDATE roles SET role_name = 'Superadmin' 
+UPDATE fda_hphc.roles SET role_name = 'Superadmin' 
 WHERE role_name = 'Project Lead';
 
-UPDATE roles SET role_name = 'Study admin' 
+UPDATE fda_hphc.roles SET role_name = 'Study admin' 
 WHERE role_name = 'Coordinator';
+
+DELETE FROM fda_hphc.user_permission_mapping WHERE permission_id IN ('5','7') AND user_id not IN
+ (SELECT user_id FROM fda_hphc.users WHERE access_level ='SUPERADMIN');
+ 
+UPDATE fda_hphc.users SET role_id='2', access_level='STUDY ADMIN'   WHERE access_level !='SUPERADMIN';
