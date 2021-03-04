ae9c4a64eae5669c76133c91d0fba6d00238358a
madhurya-btc <madhuryan@boston-technology.com>
2021-03-04 20:12:25 +0530

    security
    security

--- participant-manager-datastore/participant-manager-service/src/main/java/com/google/cloud/healthcare/fdamystudies/config/WebSecurityConfig.java
+++ participant-manager-datastore/participant-manager-service/src/main/java/com/google/cloud/healthcare/fdamystudies/config/WebSecurityConfig.java
@@ -33,7 +33,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   protected void configure(HttpSecurity http) throws Exception {
     if (appConfig.isXsrfEnabled()) {
       http.csrf().csrfTokenRepository(this.getCsrfTokenRepository());
-      http.headers();
     } else {
       http.csrf().disable();
     }
