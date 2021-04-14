d2c3b11e26d6384ea08bcb76ca23dc3c4b5b6a0e
madhurya-btc <madhuryan@boston-technology.com>
2021-04-14 18:14:00 +0530

    github-actions

--- participant-manager-datastore/participant-manager-service/src/main/java/com/google/cloud/healthcare/fdamystudies/util/ParticipantManagerUtil.java
+++ participant-manager-datastore/participant-manager-service/src/main/java/com/google/cloud/healthcare/fdamystudies/util/ParticipantManagerUtil.java
@@ -5,6 +5,7 @@
  * license that can be found in the LICENSE file or at
  * https://opensource.org/licenses/MIT.
  */
+
 package com.google.cloud.healthcare.fdamystudies.util;
 
 import com.google.cloud.healthcare.fdamystudies.config.AppPropertyConfig;
