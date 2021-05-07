52dd394f0b2ee52e0f623ac015f4727cbb91d7a8
pujithr <73532820+pujithr@users.noreply.github.com>
2021-05-07 18:10:53 +0530

    [v2.0.5] Fixed Issue #3366
    Fixed issue E#3366: Updated field from Enrollment Date to Enrollment date in DB

--- db-migration/study-builder-db-migration/db/migration/V2_0_5__release.sql
+++ db-migration/study-builder-db-migration/db/migration/V2_0_5__release.sql
@@ -1,2 +1,2 @@
-UPDATE fda_hphc.anchordate_type anchor SET anchor.name="Enrollment date" WHERE anchor.name="Enrollment Date";
+UPDATE fda_hphc.anchordate_type anchor SET anchor.name='Enrollment date' WHERE anchor.name='Enrollment Date';
 
