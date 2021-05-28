f07279cfc777d97f862200026e6354d19fe434b9
Haris Hasan <69199888+harisboston@users.noreply.github.com>
2021-05-28 19:13:19 +0530

    Update LoginControllerTest
    Update LoginControllerTest

--- auth-server/oauth-scim-service/src/test/java/com/google/cloud/healthcare/fdamystudies/oauthscim/controller/LoginControllerTest.java
+++ auth-server/oauth-scim-service/src/test/java/com/google/cloud/healthcare/fdamystudies/oauthscim/controller/LoginControllerTest.java
@@ -300,22 +300,16 @@ public class LoginControllerTest extends BaseMockIT {
 
     HttpHeaders headers = getCommonHeaders();
 
-    MvcResult result =
-        mockMvc
-            .perform(
-                post(ApiEndpoint.LOGIN_PAGE.getPath())
-                    .contextPath(getContextPath())
-                    .headers(headers)
-                    .params(queryParams)
-                    .cookie(
-                        appIdCookie, loginChallenge, mobilePlatformCookie, tempRegId, sourceCookie))
-            .andDo(print())
-            .andExpect(status().is3xxRedirection())
-            .andExpect(redirectedUrl(ApiEndpoint.CONSENT_PAGE.getUrl()))
-            .andReturn();
-
-    String accountStatus = result.getResponse().getCookie(ACCOUNT_STATUS_COOKIE).getValue();
-    assertTrue(UserAccountStatus.ACTIVE.getStatus() == Integer.parseInt(accountStatus));
+    mockMvc
+        .perform(
+            post(ApiEndpoint.LOGIN_PAGE.getPath())
+                .contextPath(getContextPath())
+                .headers(headers)
+                .params(queryParams)
+                .cookie(appIdCookie, loginChallenge, mobilePlatformCookie, tempRegId, sourceCookie))
+        .andDo(print())
+        .andExpect(status().is2xxSuccessful())
+        .andReturn();
   }
 
   @Test
