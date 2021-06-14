f730ba7f38286d055877dd934655826586e2fbc4
madhurya-btc <madhuryan@boston-technology.com>
2021-06-14 17:22:38 +0530

    #3737 issue fixed https://github.com/GoogleCloudPlatform/fda-mystudies/issues/3737

--- study-builder/fdahpStudyDesigner/src/main/java/com/fdahpstudydesigner/util/FdahpStudyDesignerUtil.java
+++ study-builder/fdahpStudyDesigner/src/main/java/com/fdahpstudydesigner/util/FdahpStudyDesignerUtil.java
@@ -32,6 +32,7 @@ import com.google.cloud.storage.StorageOptions;
 import java.math.BigDecimal;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -1125,5 +1126,16 @@ public class FdahpStudyDesignerUtil {
       logger.error("Unable to generate signed url", e);
     }
     return null;
+  }
+
+  public static boolean isValidDateFormat(String dateStr, String dateFormat) {
+    DateFormat sdf = new SimpleDateFormat(dateFormat);
+    sdf.setLenient(false);
+    try {
+      sdf.parse(dateStr);
+    } catch (ParseException e) {
+      return false;
+    }
+    return true;
   }
 }
