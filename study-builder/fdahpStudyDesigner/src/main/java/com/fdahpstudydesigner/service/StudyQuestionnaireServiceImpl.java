469b23bdcc194cb6ca71ff30b71b48fdf7663c97
navya-BTC <navyat@boston-technology.com>
2021-06-17 18:49:44 +0530

    Update StudyQuestionnaireServiceImpl.java

--- study-builder/fdahpStudyDesigner/src/main/java/com/fdahpstudydesigner/service/StudyQuestionnaireServiceImpl.java
+++ study-builder/fdahpStudyDesigner/src/main/java/com/fdahpstudydesigner/service/StudyQuestionnaireServiceImpl.java
@@ -14,7 +14,6 @@ import com.fdahpstudydesigner.bean.QuestionnaireStepBean;
 import com.fdahpstudydesigner.bo.AnchorDateTypeBo;
 import com.fdahpstudydesigner.bo.HealthKitKeysInfo;
 import com.fdahpstudydesigner.bo.InstructionsBo;
-import com.fdahpstudydesigner.bo.QuestionResponseSubTypeBo;
 import com.fdahpstudydesigner.bo.QuestionResponseTypeMasterInfoBo;
 import com.fdahpstudydesigner.bo.QuestionnaireBo;
 import com.fdahpstudydesigner.bo.QuestionnaireCustomScheduleBo;
@@ -352,11 +351,6 @@ public class StudyQuestionnaireServiceImpl implements StudyQuestionnaireService 
           }
         }
       }
-
-      List<QuestionResponseSubTypeBo> questionResponseSubTypeList =
-          studyQuestionnaireDAO.getQuestionResponseSubTypes(
-              questionnairesStepsBo.getInstructionFormId());
-      questionnairesStepsBo.setQuestionResponseSubTypeList(questionResponseSubTypeList);
 
     } catch (Exception e) {
       logger.error("StudyQuestionnaireServiceImpl - getQuestionnaireStep - Error", e);
