bb8ee5ee20b6cbea0d336c45d2dfd0ee6b597fc7
Kantharajut-btc <kantharajut@boston-technology.com>
2021-06-21 19:46:15 +0530

    Removed setting enroll again column in export
    https://github.com/GoogleCloudPlatform/fda-mystudies/issues/3022

--- study-builder/fdahpStudyDesigner/src/main/java/com/fdahpstudydesigner/service/StudyExportImportService.java
+++ study-builder/fdahpStudyDesigner/src/main/java/com/fdahpstudydesigner/service/StudyExportImportService.java
@@ -1331,8 +1331,7 @@ public class StudyExportImportService {
               customIdsMap.get(STUDY_ID + consentBo.getStudyId()),
               consentBo.getTaglineDescription(),
               consentBo.getTitle(),
-              consentBo.getVersion(),
-              consentBo.getEnrollAgain());
+              consentBo.getVersion());
       consentBoListInsertQuery.add(consentInsertSql);
     }
     insertSqlStatements.addAll(consentBoListInsertQuery);
