484b87bda97d458555e7dad7429f2099be15bb59
madhud <madhud@boston-technology.com>
2021-07-15 17:30:56 +0530

    conflict resolved

--- participant-manager/src/app/site-coordinator/mobile-menu/mobile-menu.component.ts
+++ participant-manager/src/app/site-coordinator/mobile-menu/mobile-menu.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, Output, EventEmitter, OnInit} from '@angular/core';
 import {UserService} from 'src/app/service/user.service';
+import {SearchService} from 'src/app/shared/search.service';
 import {Profile} from '../account/shared/profile.model';
 
 import {HeaderDisplayService} from '../../service/header-display.service';
@@ -14,35 +15,45 @@ export class MobileMenuComponent implements OnInit {
   navIsOpen = false;
   @Input() showSearchBar = true;
   @Input() filterQuery = '';
-  @Output() keyDown: EventEmitter<KeyboardEvent> = new EventEmitter();
+  mobileFilterQuery = '';
+  @Output() keyDown: EventEmitter<string> = new EventEmitter();
   user = {} as Profile;
   showSearchOnClick = false;
-
-  displayHeaderOnResetpassword = true;
+  updatedPlaceHolder = '';
+   displayHeaderOnResetpassword = true;
 
   constructor(
     private readonly userService: UserService,
+    private readonly searchService: SearchService,
     private readonly displayHeader: HeaderDisplayService,
-  ) { }
-
+  ) {}
   ngOnInit(): void {
     this.user = this.userService.getUserProfile();
-    this.displayHeader.showHeaders$.subscribe((updatedHeaderDisplayStatus) => {
+ this.displayHeader.showHeaders$.subscribe((updatedHeaderDisplayStatus) => {
       this.displayHeaderOnResetpassword = updatedHeaderDisplayStatus;
     });
+    this.searchService.searchPlaceHolder$.subscribe(
+      (updatedPlaceHolder: string) => {
+        this.showSearchBar = true;
+        this.mobileFilterQuery = '';
+        this.updatedPlaceHolder = updatedPlaceHolder;
+      },
+    );
   }
 
   toggleNav(): void {
     this.navIsOpen = !this.navIsOpen;
   }
- 
+
 
   hamburgerclose(): void {
     // any other execution
     this.navIsOpen = !this.navIsOpen;
   }
   mobileOnKeyDown(event: KeyboardEvent): void {
-    this.keyDown.emit(event);
+    if (event.key === 'Enter') {
+      this.keyDown.emit(this.mobileFilterQuery);
+    }
   }
   showSearchBarOnClick(): void {
     this.showSearchOnClick = true;
