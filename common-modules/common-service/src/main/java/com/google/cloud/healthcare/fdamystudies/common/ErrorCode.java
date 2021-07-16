bb9e3766ea2934ebd131dce9f45b870162083d3a
aswinijena100 <ashwinikumarj@boston-technology.com>
2021-07-16 13:33:07 +0530

    Errorcode changed
    Errorcode changed

--- common-modules/common-service/src/main/java/com/google/cloud/healthcare/fdamystudies/common/ErrorCode.java
+++ common-modules/common-service/src/main/java/com/google/cloud/healthcare/fdamystudies/common/ErrorCode.java
@@ -203,7 +203,7 @@ public enum ErrorCode {
   ADMIN_NOT_FOUND(404, "EC_0042", HttpStatus.NOT_FOUND.toString(), "Admin user not found"),
 
   PENDING_CONFIRMATION(
-      403,
+      409,
       "EC_0043",
       HttpStatus.CONFLICT.toString(),
       "Your account is pending activation. Please check your email for details and sign in to complete activation."),
