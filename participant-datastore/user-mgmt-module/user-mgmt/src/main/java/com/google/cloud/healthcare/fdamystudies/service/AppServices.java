7196f26db200269cd6d22f9ad21fbe8094fd5f79
Chiranjibi009 <chiranjibid@boston-technology.com>
2021-08-13 19:08:21 +0530

    merge issue fixes

--- participant-datastore/user-mgmt-module/user-mgmt/src/main/java/com/google/cloud/healthcare/fdamystudies/service/AppServices.java
+++
@@ -1,7 +0,0 @@
-package com.google.cloud.healthcare.fdamystudies.service;
-
-import com.google.cloud.healthcare.fdamystudies.beans.AppContactEmailsResponse;
-
-public interface AppServices {
-  AppContactEmailsResponse getAppContactEmails(String customAppId);
-}
