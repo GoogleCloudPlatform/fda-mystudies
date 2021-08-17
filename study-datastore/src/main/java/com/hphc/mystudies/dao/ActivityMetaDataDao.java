fe1d6908cb65b269ccc191fd3bd7d38a59dbc173
Haris Hasan <69199888+harisboston@users.noreply.github.com>
2021-08-17 19:36:49 +0530

    Fixed issue #4012
    https://github.com/GoogleCloudPlatform/fda-mystudies/issues/4012

--- study-datastore/src/main/java/com/hphc/mystudies/dao/ActivityMetaDataDao.java
+++ study-datastore/src/main/java/com/hphc/mystudies/dao/ActivityMetaDataDao.java
@@ -1163,11 +1163,7 @@ public class ActivityMetaDataDao {
               .createQuery(
                   "from ActiveTaskCustomFrequenciesDto ATCFDTO"
                       + " where ATCFDTO.activeTaskId=:activeTaskId"
-                      + " ORDER BY"
-                      + " case ATCFDTO.xDaysSign when '1' then ATCFDTO.timePeriodFromDays END DESC,"
-                      + " case ATCFDTO.xDaysSign when '1' then ATCFDTO.frequencyStartTime END ASC,"
-                      + " case ATCFDTO.xDaysSign when '0' then ATCFDTO.timePeriodFromDays END ASC,"
-                      + " case ATCFDTO.xDaysSign when '0' then ATCFDTO.frequencyStartTime END ASC")
+                      + " ORDER BY ATCFDTO.frequencyStartDate ASC, ATCFDTO.frequencyStartTime")
               .setString("activeTaskId", activeTask.getId())
               .list();
       if ((manuallyScheduleFrequencyList != null) && !manuallyScheduleFrequencyList.isEmpty()) {
@@ -3001,13 +2997,15 @@ public class ActivityMetaDataDao {
           }
 
           activityBean.setStartTime(
-              StudyMetaDataUtil.getFormattedDateTimeZone(
-                  startDateTime,
-                  StudyMetaDataConstants.SDF_DATE_TIME_PATTERN,
-                  StudyMetaDataConstants.SDF_DATE_TIME_TIMEZONE_MILLISECONDS_PATTERN));
+              StringUtils.isEmpty(activeTaskDto.getActiveTaskLifetimeStart())
+                  ? ""
+                  : StudyMetaDataUtil.getFormattedDateTimeZone(
+                      startDateTime,
+                      StudyMetaDataConstants.SDF_DATE_TIME_PATTERN,
+                      StudyMetaDataConstants.SDF_DATE_TIME_TIMEZONE_MILLISECONDS_PATTERN));
 
           activityBean.setEndTime(
-              StringUtils.isEmpty(endDateTime)
+              StringUtils.isEmpty(activeTaskDto.getActiveTaskLifetimeEnd())
                   ? ""
                   : StudyMetaDataUtil.getFormattedDateTimeZone(
                       endDateTime,
