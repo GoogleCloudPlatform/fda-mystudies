0d8c50e1ea30bea505f0a15ed5cd3cafae9c2251
sheelakanni97 <sheelak@boston-technology.com>
2021-11-26 12:45:40 +0530

    fixed issue - 4258
    fixed issue - 4258

--- study-builder/fdahpStudyDesigner/src/main/java/com/fdahpstudydesigner/dao/StudyQuestionnaireDAOImpl.java
+++ study-builder/fdahpStudyDesigner/src/main/java/com/fdahpstudydesigner/dao/StudyQuestionnaireDAOImpl.java
@@ -3495,19 +3495,18 @@ public class StudyQuestionnaireDAOImpl implements StudyQuestionnaireDAO {
               .getType()
               .equalsIgnoreCase(FdahpStudyDesignerConstants.ACTION_TYPE_SAVE)) {
             addOrUpdateQuestionnairesStepsBo.setStatus(false);
-
-            query =
-                session
-                    .createSQLQuery(
-                        "update questionnaires q set q.status=0 where q.id=:questionnaireId ")
-                    .setString(
-                        "questionnaireId", addOrUpdateQuestionnairesStepsBo.getQuestionnairesId());
-            query.executeUpdate();
           } else if (questionnairesStepsBo
               .getType()
               .equalsIgnoreCase(FdahpStudyDesignerConstants.ACTION_TYPE_COMPLETE)) {
             addOrUpdateQuestionnairesStepsBo.setStatus(true);
           }
+          query =
+                  session
+                      .createSQLQuery(
+                          "update questionnaires q set q.status=0 where q.id=:questionnaireId ")
+                      .setString(
+                          "questionnaireId", addOrUpdateQuestionnairesStepsBo.getQuestionnairesId());
+              query.executeUpdate();
         }
         int count = 0;
         // adding the form step to questionnaire for the first time form
@@ -3642,18 +3641,17 @@ public class StudyQuestionnaireDAOImpl implements StudyQuestionnaireDAO {
               .getType()
               .equalsIgnoreCase(FdahpStudyDesignerConstants.ACTION_TYPE_SAVE)) {
             questionnairesStepsBo.setStatus(false);
-
-            query =
-                session
-                    .createSQLQuery(
-                        "update questionnaires q set q.status=0 where q.id=:questionnairesId ")
-                    .setString("questionnairesId", questionnairesStepsBo.getQuestionnairesId());
-            query.executeUpdate();
           } else if (instructionsBo
               .getType()
               .equalsIgnoreCase(FdahpStudyDesignerConstants.ACTION_TYPE_COMPLETE)) {
             questionnairesStepsBo.setStatus(true);
           }
+          query =
+                  session
+                      .createSQLQuery(
+                          "update questionnaires q set q.status=0 where q.id=:questionnairesId ")
+                      .setString("questionnairesId", questionnairesStepsBo.getQuestionnairesId());
+              query.executeUpdate();
         }
         int count = 0;
         if ((instructionsBo.getQuestionnaireId() != null)
@@ -4363,19 +4361,18 @@ public class StudyQuestionnaireDAOImpl implements StudyQuestionnaireDAO {
               .getType()
               .equalsIgnoreCase(FdahpStudyDesignerConstants.ACTION_TYPE_SAVE)) {
             addOrUpdateQuestionnairesStepsBo.setStatus(false);
-
-            query =
-                session
-                    .createSQLQuery(
-                        "update questionnaires q set q.status=0 where q.id=:questionnairesId ")
-                    .setString(
-                        "questionnairesId", addOrUpdateQuestionnairesStepsBo.getQuestionnairesId());
-            query.executeUpdate();
           } else if (questionnairesStepsBo
               .getType()
               .equalsIgnoreCase(FdahpStudyDesignerConstants.ACTION_TYPE_COMPLETE)) {
             addOrUpdateQuestionnairesStepsBo.setStatus(true);
           }
+          query =
+                  session
+                      .createSQLQuery(
+                          "update questionnaires q set q.status=0 where q.id=:questionnairesId ")
+                      .setString(
+                          "questionnairesId", addOrUpdateQuestionnairesStepsBo.getQuestionnairesId());
+              query.executeUpdate();
         }
         int count = 0;
         if (questionnairesStepsBo.getQuestionsBo() != null) {
