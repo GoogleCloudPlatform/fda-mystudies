d9d9431888ce0c466c71410d406b3730df79b415
yugandhar-btc <yugandhare@boston-technology.com>
2022-01-19 15:52:07 +0000

    check

--- deployment/terraform/btcsoft-dev-firebase/main.tf
+++ deployment/terraform/btcsoft-dev-firebase/main.tf
@@ -198,8 +198,8 @@ resource "google_bigquery_dataset" "bq_dataset" {
   delete_contents_on_destroy = true
 }
 resource "google_pubsub_topic" "topic" {
-  name		 = "fhir-notifications"
-  project    = module.project.project_id
+  name    = "fhir-notifications"
+  project = module.project.project_id
 }
 
 resource "google_healthcare_dataset" "dataset" {
