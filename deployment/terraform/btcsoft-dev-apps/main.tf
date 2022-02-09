2e6d26848410e5f8e9eb4f62716d3747e8b13ebc
yugandhar-btc <yugandhare@boston-technology.com>
2022-02-09 03:50:26 -0500

    Disabled istio
    Disabled istio

--- deployment/terraform/btcsoft-dev-apps/main.tf
+++ deployment/terraform/btcsoft-dev-apps/main.tf
@@ -204,7 +204,7 @@ module "btcsoft_dev_gke_cluster" {
       display_name = "btcsoft-dev-ip"
     },
   ]
-  istio                   = true
+  istio                   = false
   skip_provisioners       = true
   enable_private_endpoint = false
   release_channel         = "STABLE"
