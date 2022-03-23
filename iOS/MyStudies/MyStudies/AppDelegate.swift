e9d2a1e615a427d1fbb3eb5e7fab3e91b9dddf37
GovindPrasadC <govindp@boston-technology.com>
2022-03-23 17:17:45 +0530

    - ios userdefaults change

--- iOS/MyStudies/MyStudies/AppDelegate.swift
+++ iOS/MyStudies/MyStudies/AppDelegate.swift
@@ -274,6 +274,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate, MessagingDelegate {
 
     // set Flag to handle foreground to background transition
     self.appIsResignedButDidNotEnteredBackground = false
+    let ud = UserDefaults.standard
+    ud.set(false, forKey: kPasscodeIsPending)
     blockerScreen?.isHidden = true
     blockerScreen?.removeFromSuperview()
   }
@@ -1069,7 +1071,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, MessagingDelegate {
 
     // Update User Defaults
     let ud = UserDefaults.standard
-    ud.set(false, forKey: kPasscodeIsPending)
+    ud.set(true, forKey: kPasscodeIsPending)
     ud.set(false, forKey: kShowNotification)
     ud.synchronize()
 
@@ -1105,7 +1107,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, MessagingDelegate {
       ORKPasscodeViewController.removePasscodeFromKeychain()
     }
     let ud = UserDefaults.standard
-    ud.set(false, forKey: kPasscodeIsPending)
+    ud.set(true, forKey: kPasscodeIsPending)
     ud.set(false, forKey: kShowNotification)
     ud.synchronize()
 
@@ -1543,7 +1545,7 @@ extension AppDelegate: ORKTaskViewControllerDelegate {
     case ORKTaskViewControllerFinishReason.completed:
       if !(taskViewController.task?.identifier == kConsentTaskIdentifier) {  // other surveys/Active tasks/ Passcode
         let ud = UserDefaults.standard
-        ud.set(false, forKey: kPasscodeIsPending)
+        ud.set(true, forKey: kPasscodeIsPending)
         ud.synchronize()
         self.appIsResignedButDidNotEnteredBackground = false
       }
