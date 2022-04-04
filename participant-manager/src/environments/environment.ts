d55b914ee3b050bf60df8bfceb55943c9413521c
LAPTOP-MADHUDV\Madhu D V <madhud@boston-technology.com>
2022-04-04 18:08:37 +0530

    #4437, #4482, #4483 issue fixed

--- participant-manager/src/environments/environment.ts
+++ participant-manager/src/environments/environment.ts
@@ -19,6 +19,26 @@
 //   copyright: 'Copyright 2020-2021 Google LLC.',
 // };
 
+// export const environment = {
+//   participantManagerDatastoreUrl:
+//     '//participants.btcsoft-dev.boston-technology.com/participant-manager-datastore',
+//   baseHref: '/participant-manager/',
+//   hydraLoginUrl:
+//     'https://participants.btcsoft-dev.boston-technology.com/oauth2/auth',
+//   authServerUrl:
+//     'https://participants.btcsoft-dev.boston-technology.com/auth-server',
+//   authServerRedirectUrl:
+//     'https://participants.btcsoft-dev.boston-technology.com/auth-server/callback',
+//   hydraClientId: 'TUUo9PjRQN80y3Mt',
+//   appVersion: 'v0.1',
+//   termsPageTitle: 'Terms title goes here',
+//   termsPageDescription: 'Terms description goes here',
+//   aboutPageTitle: 'About page title goes here',
+//   aboutPageDescription: 'About page description goes here',
+//   copyright: 'Copyright 2020-2021 Google LLC.',
+//   production: false,
+// };
+
 export const environment = {
   participantManagerDatastoreUrl:
     '//participants.btcsoft-dev.boston-technology.com/participant-manager-datastore',
