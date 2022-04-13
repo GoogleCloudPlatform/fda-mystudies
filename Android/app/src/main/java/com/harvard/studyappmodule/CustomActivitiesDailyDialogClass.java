044deba598b61bd65701b3250985ad5121b23b72
DESKTOP-218L00I\shashank <shashanks@boston-technology.com>
2022-04-13 12:53:39 +0530

    #4501

--- Android/app/src/main/java/com/harvard/studyappmodule/CustomActivitiesDailyDialogClass.java
+++ Android/app/src/main/java/com/harvard/studyappmodule/CustomActivitiesDailyDialogClass.java
@@ -107,7 +107,7 @@ public class CustomActivitiesDailyDialogClass extends Dialog implements View.OnC
                 } else if (finalI == 1) {
                   name = "surveys";
                 } else {
-                  name = "taskes";
+                  name = "tasks";
                 }
                 Bundle eventProperties = new Bundle();
                 eventProperties.putString(
--- Android/app/src/main/java/com/harvard/studyappmodule/consent/ConsentDocumentStepLayoutCustom.java
+++ Android/app/src/main/java/com/harvard/studyappmodule/consent/ConsentDocumentStepLayoutCustom.java
@@ -95,7 +95,7 @@ public class ConsentDocumentStepLayoutCustom extends LinearLayout implements Ste
     String htmlBase64 = Base64.encodeToString(htmlContent.getBytes(), Base64.NO_WRAP);
     pdfView.loadData(htmlBase64, "text/html", "base64");
 
-    SubmitBar submitBar = (SubmitBar) findViewById(R.id.submit_bar);
+    final SubmitBar submitBar = (SubmitBar) findViewById(R.id.submit_bar);
     submitBar.setPositiveAction(
         new Action1() {
           @Override
@@ -106,7 +106,8 @@ public class ConsentDocumentStepLayoutCustom extends LinearLayout implements Ste
                 context.getString(R.string.consent_review_agree_text));
             analyticsInstance.logEvent(
                 CustomFirebaseAnalytics.Event.ADD_BUTTON_CLICK, eventProperties);
-            ConsentDocumentStepLayoutCustom.this.showDialog();
+            submitBar.getPositiveActionView().setEnabled(false);
+            ConsentDocumentStepLayoutCustom.this.showDialog(submitBar);
           }
         });
     submitBar.setNegativeAction(
@@ -124,7 +125,7 @@ public class ConsentDocumentStepLayoutCustom extends LinearLayout implements Ste
         });
   }
 
-  private void showDialog() {
+  private void showDialog(final SubmitBar submitBar) {
     new AlertDialog.Builder(getContext())
         .setTitle(R.string.rsb_consent_review_alert_title)
         .setMessage(confirmationDialogBody)
@@ -154,6 +155,7 @@ public class ConsentDocumentStepLayoutCustom extends LinearLayout implements Ste
                 eventProperties.putString(
                     CustomFirebaseAnalytics.Param.BUTTON_CLICK_REASON,
                     context.getString(R.string.consent_agree_cancel));
+                submitBar.getPositiveActionView().setEnabled(true);
                 analyticsInstance.logEvent(
                     CustomFirebaseAnalytics.Event.ADD_BUTTON_CLICK, eventProperties);
               }
