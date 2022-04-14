12cc36962770e0a0879e4421bc0eb3bce23a8592
Haris Hasan <69199888+harisboston@users.noreply.github.com>
2022-04-14 14:36:22 +0530

    Fixed #4536
    Fixed #4536

--- study-builder/fdahpStudyDesigner/src/main/java/com/fdahpstudydesigner/dao/StudyQuestionnaireDAOImpl.java
+++ study-builder/fdahpStudyDesigner/src/main/java/com/fdahpstudydesigner/dao/StudyQuestionnaireDAOImpl.java
@@ -3661,10 +3661,9 @@ public class StudyQuestionnaireDAOImpl implements StudyQuestionnaireDAO {
           String updateQuery =
               "update QuestionnairesStepsBo QSBO set QSBO.destinationStep=:stepId "
                   + " where "
-                  + "QSBO.destinationStep="
-                  + 0
+                  + "QSBO.destinationStep='0'"
                   + " and QSBO.sequenceNo=:sequenceNo"
-                  + " and QSBO.questionnairesId=:questionnairesId ";
+                  + " and QSBO.questionnairesId=:questionnairesId";
           session
               .createQuery(updateQuery)
               .setString("stepId", questionnairesStepsBo.getStepId())
