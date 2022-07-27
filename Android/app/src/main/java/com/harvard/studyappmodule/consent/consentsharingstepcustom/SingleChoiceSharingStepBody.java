a730acac55be745f77db4d17065b7c03394b51e4
DESKTOP-218L00I\shashank <shashanks@boston-technology.com>
2022-07-27 11:42:23 +0530

    #4751 issue fixs

--- Android/app/src/main/java/com/harvard/studyappmodule/consent/consentsharingstepcustom/SingleChoiceSharingStepBody.java
+++ Android/app/src/main/java/com/harvard/studyappmodule/consent/consentsharingstepcustom/SingleChoiceSharingStepBody.java
@@ -17,9 +17,6 @@ package com.harvard.studyappmodule.consent.consentsharingstepcustom;
 
 import android.content.Intent;
 import android.content.res.Resources;
-import androidx.annotation.IdRes;
-import androidx.core.content.ContextCompat;
-
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -29,9 +26,10 @@ import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import androidx.annotation.IdRes;
+import androidx.core.content.ContextCompat;
 import com.harvard.R;
 import com.harvard.utils.CustomFirebaseAnalytics;
-
 import org.researchstack.backbone.answerformat.ChoiceAnswerFormat;
 import org.researchstack.backbone.model.Choice;
 import org.researchstack.backbone.result.StepResult;
@@ -149,6 +147,7 @@ public class SingleChoiceSharingStepBody<T> implements StepBody {
           public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
             Choice<T> choice = choices[checkedId];
             currentSelected = choice.getValue();
+            radioGroup.jumpDrawablesToCurrentState();
           }
         });
 
