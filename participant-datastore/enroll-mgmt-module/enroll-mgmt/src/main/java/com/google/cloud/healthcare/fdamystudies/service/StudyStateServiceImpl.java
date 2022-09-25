21bdfc113f0f1815de8ed09b00dee0b8d352e904
Haris Hasan <69199888+harisboston@users.noreply.github.com>
2022-09-25 12:38:02 +0530

    build

--- participant-datastore/enroll-mgmt-module/enroll-mgmt/src/main/java/com/google/cloud/healthcare/fdamystudies/service/StudyStateServiceImpl.java
+++ participant-datastore/enroll-mgmt-module/enroll-mgmt/src/main/java/com/google/cloud/healthcare/fdamystudies/service/StudyStateServiceImpl.java
@@ -92,7 +92,7 @@ public class StudyStateServiceImpl implements StudyStateService {
     logger.entry("Begin getParticipantStudiesList()");
 
     List<ParticipantStudyEntity> participantStudies = new ArrayList<>();
-    List<String> participantStudyIdsList = new ArrayList<>();
+    List<String> participantStudyIds = new ArrayList<>();
 
     List<String> customStudyIds =
         studiesBeenList
@@ -108,40 +108,23 @@ public class StudyStateServiceImpl implements StudyStateService {
             .filter(Objects::nonNull)
             .collect(Collectors.toList());
 
-    List<StudyEntity> listOfStudy = studyRepository.findByCustomIds(customStudyIds);
+    Optional<StudyEntity> optStudy = studyRepository.findByCustomIds(customStudyIds);
 
-    if (CollectionUtils.isNotEmpty(listOfStudy)) {
-      for (StudyEntity studyEntity : listOfStudy) {
-        List<String> participantStudyIds = null;
-        if (studyEntity.getType().equals(OPEN_STUDY)) {
-          participantStudyIds =
-              participantStudyRepository.findByStudyIdAndUserDetailId(
-                  studyEntity.getId(), user.getUserId());
-        } else if (CollectionUtils.isEmpty(siteIds) && studyEntity.getType().equals(CLOSE_STUDY)) {
-          participantStudyIds =
-              participantStudyRepository.findByEmailAndStudyCustomIds(
-                  user.getEmail(), customStudyIds);
-        } else {
-          participantStudyIds =
-              participantStudyRepository.findByEmailAndSiteIds(user.getEmail(), siteIds);
-        }
+    if (optStudy.isPresent() && optStudy.get().getType().equals(OPEN_STUDY)) {
+      participantStudyIds =
+          participantStudyRepository.findByStudyIdAndUserDetailId(
+              optStudy.get().getId(), user.getUserId());
+    } else if (CollectionUtils.isEmpty(siteIds) && optStudy.get().getType().equals(CLOSE_STUDY)) {
+      participantStudyIds =
+          participantStudyRepository.findByEmailAndStudyCustomIds(user.getEmail(), customStudyIds);
 
-        if (CollectionUtils.isNotEmpty(participantStudyIds)) {
-          participantStudyIdsList.addAll(participantStudyIds);
-        }
-      }
     } else {
-      participantStudyIdsList =
+      participantStudyIds =
           participantStudyRepository.findByEmailAndSiteIds(user.getEmail(), siteIds);
     }
 
-    if (CollectionUtils.isNotEmpty(participantStudyIdsList)) {
-      List<ParticipantStudyEntity> participantStudiesList =
-          participantStudyRepository.findAllById(participantStudyIdsList);
-
-      if (CollectionUtils.isNotEmpty(participantStudiesList)) {
-        participantStudies.addAll(participantStudiesList);
-      }
+    if (CollectionUtils.isNotEmpty(participantStudyIds)) {
+      participantStudies = participantStudyRepository.findAllById(participantStudyIds);
     }
 
     logger.exit("getParticipantStudiesList() - Ends ");
@@ -200,7 +183,6 @@ public class StudyStateServiceImpl implements StudyStateService {
         participantStudyEntity.setCompletion(studyBean.getCompletion());
         participantStudyEntity.setAdherence(studyBean.getAdherence());
         participantStudyEntity.setUserDetails(user);
-        participantStudyEntity.setUserStudyVersion(studyBean.getUserStudyVersion());
 
         placeHolder.put("study_state_value", participantStudyEntity.getStatus());
         participantStudies.add(participantStudyEntity);
@@ -265,7 +247,6 @@ public class StudyStateServiceImpl implements StudyStateService {
         studyStateBean.setCompletion(participantStudy.getCompletion());
         studyStateBean.setBookmarked(participantStudy.getBookmark());
         studyStateBean.setAdherence(participantStudy.getAdherence());
-        studyStateBean.setUserStudyVersion(participantStudy.getUserStudyVersion());
         studyStateBean.setDataSharingPermission(participantStudy.getSharing());
         if (participantStudy.getEnrolledDate() != null) {
           studyStateBean.setEnrolledDate(
