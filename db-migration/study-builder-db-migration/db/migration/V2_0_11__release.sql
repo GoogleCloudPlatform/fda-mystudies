0ac4baca949593e2810476f8f7a3324afc24a59e
AKASH KUMAR VERMA <akashkumarv@boston-technology.com>
2022-10-20 11:27:01 +0530

    SB db migration
    SB db migration

--- db-migration/study-builder-db-migration/db/migration/V2_0_11__release.sql
+++ db-migration/study-builder-db-migration/db/migration/V2_0_11__release.sql
@@ -2,3 +2,5 @@
    and update text accordingly. */
    
 ALTER TABLE `fda_hphc`.`consent` ADD COLUMN `signature_Removal` BIT(1) NULL DEFAULT NULL;
+
+ALTER TABLE `fda_hphc`.`studies` ADD COLUMN `destination_custom_study_id` VARCHAR(255) NULL DEFAULT NULL COLLATE 'utf8_general_ci';
